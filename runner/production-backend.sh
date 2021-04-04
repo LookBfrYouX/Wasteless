318dc0ae61fc189162216f70482f4a33b0f579ee
Rio Ogino <rog19@uclive.ac.nz>
2021-04-04 15:37:27 +1200

    S302T300-112 #time 25m ?chore DGAA now uses environment variables

--- runner/production-backend.sh
+++ runner/production-backend.sh
@@ -5,4 +5,4 @@
 source ./env.txt
 
 fuser -k 8999/tcp || true
-java -jar production-backend/libs/backend-0.0.1-SNAPSHOT.jar --server.port=8999
+java -jar production-backend/libs/backend-0.0.1-SNAPSHOT.jar --server.port=8999 -Dspring.profiles.active=production
--- runner/staging-backend.sh
+++ runner/staging-backend.sh
@@ -5,4 +5,4 @@
 source ./env.txt
 
 fuser -k 9499/tcp || true
-java -jar staging-backend/libs/backend-0.0.1-SNAPSHOT.jar --server.port=9499
+java -jar staging-backend/libs/backend-0.0.1-SNAPSHOT.jar --server.port=9499 -Dspring.profiles.active=staging
