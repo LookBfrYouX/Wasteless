d48ae4d407881d4cfe053f7531f4ca5c652eb32b
Rio Ogino <rog19@uclive.ac.nz>
2021-04-04 19:47:56 +1200

    S302T15-112 #time 5m ?chore ?pair[rog19, amf133] more testing
    The production/staging environments weren't actually being used

--- runner/staging-backend.sh
+++ runner/staging-backend.sh
@@ -4,5 +4,8 @@
 
 source ./env.txt
 
+# Makes Spring use application-production.properties
+export SPRING_PROFILES_ACTIVE=staging
+
 fuser -k 9499/tcp || true
-java -jar staging-backend/libs/backend-0.0.1-SNAPSHOT.jar --server.port=9499 -Dspring.profiles.active=staging
+java -jar staging-backend/libs/backend-0.0.1-SNAPSHOT.jar --server.port=9499
