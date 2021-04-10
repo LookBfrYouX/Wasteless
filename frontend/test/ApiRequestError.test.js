968e237a07933bcdd1e2513ea5149f0a4f412920
Rio Ogino <rog19@uclive.ac.nz>
2021-04-10 19:46:54 +1200

    S302T300-122 #time 30m ?test add tests for ApiRequestError

---
+++ frontend/test/ApiRequestError.test.js
@@ -0,0 +1,53 @@
+import { ApiRequestError }  from "./../src/ApiRequestError";
+
+const customMessages = {
+  [ApiRequestError.NO_RESPONSE_KEY]: "A",
+  [ApiRequestError.DEFAULT_KEY]: "B",
+  401: "401",
+  200: "200"
+};
+
+const getError = code => ({response: {status: code}});
+
+describe("createFromMessageMap", () => {
+  test("no response default handler", () => {
+    expect(ApiRequestError.createFromMessageMap(undefined, {}).userFacingErrorMessage)
+    .toEqual(ApiRequestError.DEFAULT_MESSAGES_MAP[ApiRequestError.NO_RESPONSE_KEY]);
+  });
+  test("no response given handler", () => {
+    expect(ApiRequestError.createFromMessageMap(undefined, customMessages).userFacingErrorMessage)
+    .toEqual("A");
+  });
+
+  test("unknown response default handler", () => {
+    expect(ApiRequestError.createFromMessageMap(getError(1000), {}).userFacingErrorMessage)
+    .toEqual(ApiRequestError.DEFAULT_MESSAGES_MAP[ApiRequestError.DEFAULT_KEY](getError(1000)));
+  });
+
+  test("unknown response given default handler", () => {
+    expect(
+        ApiRequestError.createFromMessageMap(getError(1000), {
+        [ApiRequestError.DEFAULT_KEY]: "Z"
+        }).userFacingErrorMessage
+    ).toEqual("Z");
+  });
+  test("unknown response given handler", () => {
+    expect(ApiRequestError.createFromMessageMap(getError(200), customMessages).userFacingErrorMessage)
+    .toEqual("200");
+  });
+
+  test("known response default handler", () => {
+    expect(ApiRequestError.createFromMessageMap(getError(401), {}).userFacingErrorMessage)
+    .toEqual(ApiRequestError.DEFAULT_MESSAGES_MAP[401]);
+  });
+
+  test("known response given handler", () => {
+    expect(ApiRequestError.createFromMessageMap(getError(401), customMessages).userFacingErrorMessage)
+    .toEqual("401");
+  });
+
+  test("This should fail", () => {
+    expect(ApiRequestError.createFromMessageMap(getError(401), customMessages).userFacingErrorMessage)
+    .toEqual("FAIL");
+  });
+});
