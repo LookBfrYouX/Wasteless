5223f695028ed0b1bf58e2d8ffd36dbfe3d81e61
nbt25 <nbt25@uclive.ac.nz>
2021-04-11 21:09:22 +1200

    S302T300-123 ?time 30m ?chore fix sonarqube

--- frontend/sonarqube-analysis.js
+++ frontend/sonarqube-analysis.js
@@ -10,10 +10,9 @@ sonarqubeScanner(
         'sonar.sources': 'src',
         'sonar.tests': 'src',
         'sonar.inclusions': '**',
-        'sonar.test.inclusions': 'src/**/*.spec.js,src/**/*.test.js,src/**/*.test.ts, src/test',
+        'sonar.test.inclusions': 'src/**/*.spec.js,src/**/*.test.js,src/**/*.test.ts',
         'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
         'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
         'sonar.testExecutionReportPaths': 'coverage/test-reporter.xml'
       }
-    }, () => {
-    });
+    }, () => {});
