4237b09ed7726a11166b1f2fa770900c12920bbe
Maximilian Birzer <mbi47@uclive.ac.nz>
2021-04-13 11:18:31 +1200

    S302T300-124 #time 5m ?test -testing on staging server x7

--- runner/staging-backend.sh
+++ runner/staging-backend.sh
@@ -5,7 +5,7 @@
 source ./env.txt
 
 # Makes Spring use application-production.properties
-export SPRING_PROFILES_ACTIVE=staging
+# export SPRING_PROFILES_ACTIVE=staging
 
 fuser -k 9499/tcp || true
-java -jar staging-backend/libs/backend-0.0.1-SNAPSHOT.jar --server.port=9499
+java -jar staging-backend/libs/backend-0.0.1-SNAPSHOT.jar --server.port=9499 --spring.profiles.active=staging
