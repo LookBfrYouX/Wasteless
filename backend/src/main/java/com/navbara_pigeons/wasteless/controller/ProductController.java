33f8588a65cfb5de8a402fc34d4044c699dfc576
Maximilian Birzer <mbi47@uclive.ac.nz>
2021-04-13 14:08:21 +1200

    S302T300-35 #time 10m ?implement -started creating controller for U15 Products

---
+++ backend/src/main/java/com/navbara_pigeons/wasteless/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.navbara_pigeons.wasteless.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * This controller class provides the endpoints for dealing with products. All
+ */
+@Controller
+@Slf4j
+@RequestMapping("")
+public class ProductController {
+
+
+
+}
