1a987f3fe6766f69e2cc8eda9f440196a5e5387f
Maximilian Birzer <mbi47@uclive.ac.nz>
2021-04-13 12:42:44 +1200

    S302T300-124 #time 5m ?test -testing on staging server x15

--- runner/staging-backend.sh
+++ runner/staging-backend.sh
@@ -2,8 +2,6 @@
 
 # Run the staging backend server
 
-echo 'DB_USERNAME=seng302-team300' >> ./env.txt
-echo 'DB_PASSWORD=LowestVaries9525' >> ./env.txt
 source ./env.txt
 
 # Makes Spring use application-production.properties
