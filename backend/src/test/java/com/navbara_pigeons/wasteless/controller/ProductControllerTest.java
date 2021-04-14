23f79d15729f6be05529dacb0d80de8540e4d56b
Alec Fox <amf133@uclive.ac.nz>
2021-04-14 13:49:13 +1200

    S302T300-58 #time 30m ?test ?pair[amf133, jpy19] Tests for product controller - Created skeleton tests for the product controller - Product entity needs to be first created for these to pass

--- backend/src/test/java/com/navbara_pigeons/wasteless/controller/ProductControllerTest.java
+++ backend/src/test/java/com/navbara_pigeons/wasteless/controller/ProductControllerTest.java
@@ -1,25 +1,53 @@
 package com.navbara_pigeons.wasteless.controller;
 
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = ProductController.class)
 public class ProductControllerTest {
 
     @Autowired
     public MockMvc mockMvc;
 
+    // TODO: create product entity
+    @MockBean
+    private Product product;
+
     @Autowired
-    private WebApplicationContext wac;
+    private ObjectMapper objectMapper;
 
+    // Throws a 400 status code on a bad request to the controller
     @Test
-    public void showAllBusinessProductsTest() {
+    public void throwErrorOnBadProductTest() throws Exception {
+        // TODO: This product might need to be created before these tests will pass
+        Product product = new Product(null, null, 3, "Tony");
 
+        mockMvc.perform(post("/businesses/{id}/products")
+            .contentType("application/json")
+            .param("businessId", "true")
+            .content(objectMapper.writeValueAsString(product)))
+            .andExpect(status().isBadRequest());
     }
 
+    // Throws a 201 status code on a successful request to the controller
+    @Test
+    public void return201OnAddProductTest() throws Exception {
+        // TODO: This product might need to be created before these tests will pass
+        Product product = new Product("WATT-420-BEANS", "Watties Baked Beans - 420g can", "Baked Beans as they should be.", 2.2);
+
+        mockMvc.perform(post("/businesses/{id}/products")
+            .contentType("application/json")
+            .param("businessId", "true")
+            .content(objectMapper.writeValueAsString(product)))
+            .andExpect(status().isCreated());
+    }
 }
