4b2acafc560632fe2b6e7fde08bad5d872206ba3
Jordan Pyott <jpy19@uclive.ac.nz>
2021-04-14 14:14:05 +1200

    S302T300-132 #time 15m ?implement product exception ?pair[amf133, jpy19]
    - Created custom exception for product endpoint

--- backend/src/main/java/com/navbara_pigeons/wasteless/controller/ProductController.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
+import com.navbara_pigeons.wasteless.exception.ProductRegistrationException;
 
 /**
  * This controller class provides the endpoints for dealing with products. All requests for products
