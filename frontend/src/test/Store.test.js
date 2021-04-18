ba2a6a04fc6a5a3e0ab199bee0aaeda4e30cd670
Haruka Ichinose <hic21@uclive.ac.nz>
2021-04-18 17:05:12 +1200

    S302T300-88 #time 1h ?test ?pair[mbi47, hic21] Wrote tests for actingAs in store.js

---
+++ frontend/src/test/Store.test.js
@@ -0,0 +1,22 @@
+import { store } from '../store'
+
+describe("Setting actingAs", () => {
+  test("Set a business", () => {
+    const business = {
+      id: 1,
+      name: "Haruka's Hamburgers",
+    }
+    store.actions.setActingAs(business);
+    expect(store.getters.getActingAs().name).toEqual(business.name);
+  });
+
+  test("Delete a business and switch back to individual", () => {
+    const business = {
+      id: 1,
+      name: "Haruka's Hamburgers",
+    }
+    store.actions.setActingAs(business);
+    store.actions.deleteActingAS();
+    expect(store.getters.getActingAs()).toEqual(null);
+  })
+})
