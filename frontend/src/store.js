9f15b6cdae71d74023b03369c4d55fe6c749c40e
nbt25 <nbt25@uclive.ac.nz>
2021-04-22 08:33:09 +1200

    S302T300-67 #time 30m ?fix persistant acting as storage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,7 @@ import Vue from 'vue';
 const state = new Vue.observable({
   authUser: JSON.parse(localStorage.getItem("authUser")),
   welcomeMessage: "HERE WE GOOO",
-  actingAs: null
+  actingAs: JSON.parse(localStorage.getItem("actingAs"))
 });
 
 // This is the globally accessible store. It allows the state to be read and modified.
@@ -69,10 +69,12 @@ export const store = {
 
     setActingAs: (business) => {
       state.actingAs = business;
+      localStorage.setItem("actingAs", JSON.stringify(state.actingAs));
     },
 
     deleteActingAs: () => {
       state.actingAs = null;
+      localStorage.setItem("actingAs", JSON.stringify(state.actingAs));
     }
   },
 };
