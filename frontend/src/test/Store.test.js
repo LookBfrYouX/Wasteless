187c5919f4e492ffff08e1897fd4fbfc6e805777
Rio Ogino <rog19@uclive.ac.nz>
2021-04-21 13:56:31 +1200

    S302T300-89 #time 4m ?test ?pair[rog19, hic21] fix state store tests

--- frontend/src/test/Store.test.js
+++ frontend/src/test/Store.test.js
@@ -1,20 +1,25 @@
 import { store } from '../store'
 
+const business = {
+  id: 1,
+  name: "Haruka's Hamburgers",
+};
+const user = {
+  id: 10,
+  businesses: [
+    business
+  ]
+};
+
 describe("Setting actingAs", () => {
   test("Set a business", () => {
-    const business = {
-      id: 1,
-      name: "Haruka's Hamburgers",
-    }
+    store.actions.setAuthUser(user);
     store.actions.setActingAs(business);
-    expect(store.getters.getActingAs().name).toEqual(business.name);
+    expect(store.getters.getActingAs()).toEqual(business);
   });
 
   test("Delete a business and switch back to individual", () => {
-    const business = {
-      id: 1,
-      name: "Haruka's Hamburgers",
-    }
+    store.actions.setAuthUser(user);
     store.actions.setActingAs(business);
     store.actions.deleteActingAs();
     expect(store.getters.getActingAs()).toEqual(null);
