51f4f4d3c4331cfa0876dadf8f3d8a78f8464101
dnb36 <dnb36@uclive.ac.nz>
2021-04-22 16:30:51 +1200

    S302T300-85 #time 1m0 ?chore removed all this as someone did it on another task

--- frontend/src/components/BusinessProfile.vue
+++ frontend/src/components/BusinessProfile.vue
@@ -25,78 +25,7 @@
           <dt class="col-md label">Business Type:</dt>
           <dd class="col-md value"> {{ businessInfo.businessType }}</dd>
         </li>
-        <li class="row" v-if="Array.isArray(products) && products.length !== 0">
-          <dt class="col-md label">Products:</dt>
-        </li>
       </ul>
-      <div v-if="products.length != 0" class="w-100">
-        <div v-if="!isVisible" class="button-expand-sidebar-wrapper">
-          <button class="btn btn-info mt-2" type="button" v-on:click="toggleSidebar()">
-            <span>Sort products</span>
-          </button>
-        </div>
-        <div class="p-relative w-100 d-flex align-items-stretch">
-          <div v-if="isVisible" class="sort-products bg-light">
-            <div class="p-3">
-              <h3 class="d-inline">Sort by</h3>
-              <button class="float-right btn btn-light" type="button" v-on:click="toggleSidebar()">
-                <span>&larr;</span>
-              </button>
-              <ul id="search-headers" class="list-unstyled"
-                  v-bind:class='{"table-reversed": reversed}'>
-                <li
-                    v-for="[key, value] in Object.entries({name: 'Product Name', recommendedRetailPrice: 'Price', created: 'Date Created', id: 'Product Code'})"
-                    v-bind:key="key"
-                    class="mb-1"
-                    v-bind:class='{"current-sort": sortBy==key}'
-                    v-on:click="sortByClicked(key)"
-                > {{ value }}
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div v-if="isVisible" class="overlay w-100 d-md-none" v-on:click="toggleSidebar()"></div>
-          <div class="products-content container d-flex justify-content-end justify-content-md-center">
-            <div class="products-wrapper col-12 col-md-8 mt-5">
-              Displaying products {{ 10 * this.pageNum + 1 }} -
-              {{ Math.min(products.length, 10 * (this.pageNum + 1)) }}
-              out of {{ products.length }}
-              <ul class="list-unstyled list-group">
-                <li v-for="(product, index) in displayedProducts" v-bind:key="index"
-                    class="list-group-item card">
-                  <div class="d-flex flex-wrap justify-content-between">
-                    <h4 class="card-title mb-0">{{ product.name }}</h4>
-                    <span class="text-muted">{{ product.recommendedRetailPrice }}</span>
-                  </div>
-                  <div class="text-muted">{{ product.description }}</div>
-                </li>
-              </ul>
-              <div aria-label="table-nav" class="mt-2">
-                <ul class="paginate list-unstyled d-flex justify-content-center">
-                  <li class="pageItem">
-                    <button v-if="pageNum > 0" class="page-link" name="button" type="button"
-                            v-on:click="pageNum--">Previous
-                    </button>
-                  </li>
-                  <!--number of buttons scale to amount of pages-->
-                  <li v-for="pageNumber in pages.slice(Math.max(0, pageNum - 1), pageNum + 5)"
-                      :key="pageNumber"
-                      class="page-item">
-                    <button class="page-link" name="button" type="button"
-                            v-on:click="pageNum=pageNumber">{{ pageNumber + 1 }}
-                    </button>
-                  </li>
-                  <li>
-                    <button v-if="pageNum < pages.length - 1" class="page-link" name="button"
-                            type="button" v-on:click="pageNum++">Next
-                    </button>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       <div v-if="errorMessage.length > 0" class="row mt-2">
         <div class="col">
           <p class="alert alert-warning">{{ errorMessage }}</p>
@@ -129,13 +58,6 @@ export default {
         homeAddress: "",
         businessType: "",
       },
-      products: [],
-      pageNum: 0, // Page number starts from 0 but it will shown as 1 on UI
-      productsPerPage: 10,
-      pages: [],
-      sortBy: null,
-      reversed: false,
-      isVisible: false,
       errorMessage: "",
     }
   },
@@ -150,7 +72,6 @@ export default {
   beforeMount: function () {
     // gets user information from api
     this.parseApiResponse(this.callApi(this.businessId))
-    this.getProducts()
   },
 
   methods: {
@@ -179,82 +100,13 @@ export default {
             err.userFacingErrorMessage == undefined ? err.toString() : err.userFacingErrorMessage);
       }
     },
-
-    /**
-     * Calls the API to get product information with the current business ID
-     */
-    getProducts: async function () {
-      try {
-        const response = await Api.getProducts(this.businessId);
-        console.log(response.data);
-        this.products = response.data;
-      } catch (err) {
-        alert(
-            err.userFacingErrorMessage == undefined ? err.toString() : err.userFacingErrorMessage);
-      }
-    },
-    toggleSidebar() {
-      this.isVisible = !this.isVisible;
-    },
-    sortByClicked(newSortBy) {
-      if (this.sortBy == newSortBy) {
-        this.reversed = !this.reversed;
-      }
-      this.sortBy = newSortBy;
-    },
-    setPages() {
-      /* calculates number of pages which is reliant on productsPerPage set in the data section*/
-      let numOfPages = Math.ceil(this.products.length / this.productsPerPage);
-      this.pages = [];
-      for (let i = 0; i < numOfPages; i++) {
-        this.pages.push(i);
-      }
-    },
-
-    paginate(products) {
-      let page = this.pageNum;
-      let productsPerPage = this.productsPerPage;
-      let from = page * productsPerPage;
-      let to = from + productsPerPage;
-      return products.slice(from, to);
-    },
-
-    sortProducts() {
-      if (this.sortBy == null) {
-        return this.products;
-      }
-
-      let formatter = product => {
-        if (product[this.sortBy] == null) {
-          return "";
-        }
-        return product[this.sortBy].toLowerCase();
-      }
-
-      return this.products.sort((a, b) => { // sort using this.orderBy
-        return (this.reversed ? -1 : 1) * (formatter(a) > formatter(b) ? 1 : -1);
-      })
-    }
   },
 
-  computed: {
-    sortedProducts: function () {
-      return this.sortProducts()
-    },
-
-    displayedProducts() {
-      return this.paginate(this.sortedProducts);
-    }
-  },
+  computed: {},
 
   watch: {
     businessId: function () {
       this.parseApiResponse(this.callApi(this.businessId))
-    },
-    products: function () {
-      this.pageNum = 0;
-      this.setPages();
-      this.getProducts()
     },
   },
 }
