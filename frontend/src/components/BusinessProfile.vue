898a0227a0742066e8ac776e634e6733647db025
dnb36 <dnb36@uclive.ac.nz>
2021-04-22 16:00:44 +1200

    S302T300-85 #time 2h ?implement changed products to have pages/sorting, similar to searchresults

--- frontend/src/components/BusinessProfile.vue
+++ frontend/src/components/BusinessProfile.vue
@@ -29,12 +29,73 @@
           <dt class="col-md label">Products:</dt>
         </li>
       </ul>
-      <div class="card my-2" v-for="(product, index) in products"
-           v-bind:key="index">
-        <div class="card-body">
-          <h5 class="card-title">{{ product.name }}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">{{ product.recommendedRetailPrice }}</h6>
-          <p class="card-text">{{ product.description }}</p>
+      <div v-if="products.length != 0" class="w-100">
+        <div v-if="!isVisible" class="button-expand-sidebar-wrapper">
+          <button class="btn btn-info mt-2" type="button" v-on:click="toggleSidebar()">
+            <span>Sort products</span>
+          </button>
+        </div>
+        <div class="p-relative w-100 d-flex align-items-stretch">
+          <div v-if="isVisible" class="sort-results bg-light">
+            <div class="p-3">
+              <h3 class="d-inline">Sort by</h3>
+              <button class="float-right btn btn-light" type="button" v-on:click="toggleSidebar()">
+                <span>&larr;</span>
+              </button>
+              <ul id="search-headers" class="list-unstyled"
+                  v-bind:class='{"table-reversed": reversed}'>
+                <li
+                    v-for="[key, value] in Object.entries({name: 'Product Name', recommendedRetailPrice: 'Price', created: 'Date Created', id: 'Product Code'})"
+                    v-bind:key="key"
+                    class="mb-1"
+                    v-bind:class='{"current-sort": sortBy==key}'
+                    v-on:click="sortByClicked(key)"
+                > {{ value }}
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div v-if="isVisible" class="overlay w-100 d-md-none" v-on:click="toggleSidebar()"></div>
+          <div class="results-content container d-flex justify-content-end justify-content-md-center">
+            <div class="results-wrapper col-12 col-md-8 mt-5">
+              Displaying products {{ 10 * this.pageNum + 1 }} -
+              {{ Math.min(products.length, 10 * (this.pageNum + 1)) }}
+              out of {{ products.length }}
+              <ul class="list-unstyled list-group">
+                <!--viewUser method uses router.push to display profile page-->
+                <li v-for="(product, index) in displayedProducts" v-bind:key="index"
+                    class="list-group-item card">
+                  <div class="d-flex flex-wrap justify-content-between">
+                    <h4 class="card-title mb-0">{{ product.name }}</h4>
+                    <span class="text-muted">{{ product.recommendedRetailPrice }}</span>
+                  </div>
+                  <div class="text-muted">{{ product.description }}</div>
+                </li>
+              </ul>
+              <div aria-label="table-nav" class="mt-2">
+                <ul class="paginate list-unstyled d-flex justify-content-center">
+                  <li class="pageItem">
+                    <button v-if="pageNum > 0" class="page-link" name="button" type="button"
+                            v-on:click="pageNum--">Previous
+                    </button>
+                  </li>
+                  <!--number of buttons scale to amount of pages-->
+                  <li v-for="pageNumber in pages.slice(Math.max(0, pageNum - 1), pageNum + 5)"
+                      :key="pageNumber"
+                      class="page-item">
+                    <button class="page-link" name="button" type="button"
+                            v-on:click="pageNum=pageNumber">{{ pageNumber + 1 }}
+                    </button>
+                  </li>
+                  <li>
+                    <button v-if="pageNum < pages.length - 1" class="page-link" name="button"
+                            type="button" v-on:click="pageNum++">Next
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div v-if="errorMessage.length > 0" class="row mt-2">
@@ -70,6 +131,12 @@ export default {
         businessType: "",
       },
       products: [],
+      pageNum: 0, // Page number starts from 0 but it will shown as 1 on UI
+      resultsPerPage: 10,
+      pages: [],
+      sortBy: null,
+      reversed: false,
+      isVisible: false,
       errorMessage: "",
     }
   },
@@ -127,15 +194,69 @@ export default {
             err.userFacingErrorMessage == undefined ? err.toString() : err.userFacingErrorMessage);
       }
     },
+    toggleSidebar() {
+      this.isVisible = !this.isVisible;
+    },
+    sortByClicked(newSortBy) {
+      if (this.sortBy == newSortBy) {
+        this.reversed = !this.reversed;
+      }
+      this.sortBy = newSortBy;
+    },
+    setPages() {
+      /* calculates number of pages which is reliant on resultsPerPage set in the data section*/
+      let numOfPages = Math.ceil(this.products.length / this.resultsPerPage);
+      this.pages = [];
+      for (let i = 0; i < numOfPages; i++) {
+        this.pages.push(i);
+      }
+    },
+
+    paginate(results) {
+      let page = this.pageNum;
+      let resultsPerPage = this.resultsPerPage;
+      let from = page * resultsPerPage;
+      let to = from + resultsPerPage;
+      return results.slice(from, to);
+    },
+
+    sortProducts() {
+      if (this.sortBy == null) {
+        return this.products;
+      }
+
+      let formatter = product => {
+        if (product[this.sortBy] == null) {
+          return "";
+        }
+        return product[this.sortBy].toLowerCase();
+      }
+
+      return this.products.sort((a, b) => { // sort using this.orderBy
+        return (this.reversed ? -1 : 1) * (formatter(a) > formatter(b) ? 1 : -1);
+      })
+    }
   },
 
-  computed: {},
+  computed: {
+    sortedProducts: function () {
+      return this.sortProducts()
+    },
+
+    displayedProducts() {
+      return this.paginate(this.sortedProducts);
+    }
+  },
 
   watch: {
     businessId: function () {
       this.parseApiResponse(this.callApi(this.businessId))
+    },
+    products: function () {
+      this.pageNum = 0;
+      this.setPages();
       this.getProducts()
-    }
+    },
   },
 }
 </script>
