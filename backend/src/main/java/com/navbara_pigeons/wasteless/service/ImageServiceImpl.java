7fedfc696a6bc2ab8e6fc71c4ddd35306cdf43f5
Fletcher Dick <fdi19@uclive.ac.nz>
2021-04-26 10:30:18 +1200

    S302T300-32 #time 25m Finaly succeded in saving the filename against user in DB! ?implement - Added the Transactional Annotation to the method

--- backend/src/main/java/com/navbara_pigeons/wasteless/service/ImageServiceImpl.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/service/ImageServiceImpl.java
@@ -6,6 +6,8 @@ import com.navbara_pigeons.wasteless.entity.User;
 import com.navbara_pigeons.wasteless.exception.UserNotFoundException;
 import java.io.File;
 import java.io.IOException;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,20 +21,24 @@ public class ImageServiceImpl implements ImageService {
 
   private final UserDao userDao;
   private final ImageDao imageDao;
+  private final String userPrefix = "U";
+  private final String productPrefix = "P";
 
+  @Autowired
   public ImageServiceImpl(UserDao userDao, ImageDao imageDao) {
     this.userDao = userDao;
     this.imageDao = imageDao;
   }
 
   /**
-   * Upload a given profile image to the machine and store the image name in the database
+   * Upload a given profile image to the machine and store the image name in the database.
    *
    * @param image The image file to be saved
    * @return The URI to access the saved image
    * @throws UserNotFoundException The user could not be found from the Session
    * @throws IOException           The new filename could not be created
    */
+  @Transactional
   public String uploadProfileImage(MultipartFile image)
       throws UserNotFoundException, IOException {
     // Get the User object so we can use their ID
@@ -45,14 +51,13 @@ public class ImageServiceImpl implements ImageService {
     String fileExtension = fileName.substring(fileName.lastIndexOf(".") + 1);
 
     // Generate a new random filename prefixed with 'U{userId}_'
-    String newFileName = File.createTempFile("U" + currentUser.getId() + "_", "." + fileExtension)
+    String newFileName = File
+        .createTempFile(userPrefix + currentUser.getId() + "_", "." + fileExtension)
         .getName();
 
     // Save the image in the images/user/ directory then save image name in DB
     imageDao.saveProfileImageToMachine(image, newFileName);
     currentUser.setImageName(newFileName);
-    userDao.saveUser(currentUser);
-
 
     // Return the URI for to download the image
     return ServletUriComponentsBuilder.fromCurrentContextPath()
