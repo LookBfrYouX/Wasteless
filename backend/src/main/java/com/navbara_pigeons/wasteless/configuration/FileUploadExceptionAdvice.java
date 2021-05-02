2f0646f4602ecb83e69980026f873fbb2ed151fd
Alec Fox <amf133@uclive.ac.nz>
2021-05-02 23:51:48 +1200

    S302T300-146 #time 1m added javadoc ?chore

--- backend/src/main/java/com/navbara_pigeons/wasteless/configuration/FileUploadExceptionAdvice.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/configuration/FileUploadExceptionAdvice.java
@@ -7,9 +7,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+/**
+ * Global error handling middleware for file uploads
+ */
 @ControllerAdvice
 public class FileUploadExceptionAdvice extends ResponseEntityExceptionHandler {
 
+  /**
+   * Whenever MaxUploadSizeExceededException is thrown, this class will be run
+   *
+   * @return Response to the user (BAD_REQUEST)
+   */
   @ExceptionHandler(MaxUploadSizeExceededException.class)
   public ResponseEntity<String> handleMaxSizeException() {
     return new ResponseEntity<>("File too large!", HttpStatus.BAD_REQUEST);
