17ac45c91ad45fd3013d73bf5cee388543d8b40d
Maximilian Birzer <mbi47@uclive.ac.nz>
2021-05-11 12:42:29 +1200

    S302T300-171 #time 2h ?implement Create ItemCard component - Added basic template for ItemCardContainer.vue and some dummy data. - Added basic template for itemSorter.vue and added to container. - Added basic template for ItemCard.vue and displayed this in the container.

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -2,10 +2,13 @@
   <div class="container pt-4">
     <h1>Home Page</h1>
     <p>Beautiful, ain't it?</p>
+    <item-card-container />
   </div>
 </template>
 <script>
+import ItemCardContainer from "./containers/ItemCardContainer";
 export default {
-  name: "home"
+  name: "home",
+  components: {ItemCardContainer}
 }
 </script>
---
+++ frontend/src/components/cards/ItemCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- this component produces the item card component from the given props -->
+<div>
+  <h1>{{ item.title }}</h1>
+  <h3>${{ item.totalPrice }} NZD</h3>
+  <p>{{ item.description }}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "ItemCard",
+  props: {
+    item: Object,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
