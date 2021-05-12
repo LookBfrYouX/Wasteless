e8c2052f5bb081536b6487e29758d0f37b843718
Rio Ogino <rog19@uclive.ac.nz>
2021-05-12 22:15:54 +1200

    S302T300-74 #time 30m ?implement minor sidebar design update
    - Close arrow is now an X
    - Up/down arrow is now larger and more visible
    - Up/down arrow animates
    - Divider between sort elements
    - Box shadow on hover

--- frontend/src/components/SortSidebar.vue
+++ frontend/src/components/SortSidebar.vue
@@ -3,30 +3,49 @@
     <div class="p-3">
       <h3 class="d-inline">Sort By:</h3>
       <button class="float-right btn btn-light d-flex" type="button" v-on:click="closeClicked()">
-        <span class="material-icons">arrow_back</span>
+        <span class="material-icons">clear</span>
       </button>
-      <ul id="search-headers" class="list-unstyled"
-          v-bind:class='{"table-reversed": currentSortOption.reversed}'>
+      <ul
+        id="search-headers"
+        class="list-unstyled w-100 mt-2"
+        v-bind:class="{'table-reversed': currentSortOption.reversed}"
+      >
         <li
-            v-for="sortOption in sortOptions"
+            v-for="(sortOption, i) in sortOptions"
             v-bind:key="sortOption.name"
-            class="mb-1"
+            class="w-100"
             v-bind:class='{"current-sort": currentSortOption.name == sortOption.name}'
             v-on:click="sortByClicked(sortOption)"
-        > {{ sortOption.name }}
+        > 
+          <div class="dropdown-divider" v-if="i"></div>
+          <div class="w-100 sort-text rounded p-1 d-flex flex-row justify-content-between align-items-center">
+            <span>{{ sortOption.name }}</span>
+            <span class="arrow material-icons user-select-none">arrow_upward</span>
+          </div>
         </li>
       </ul>
     </div>
   </div>
 </template>
 <style scoped>
-.sort-results li.current-sort::after {
-  content: '\2191';
+#search-headers li .sort-text:hover {
+  /* background-color: white; */
+  box-shadow: 0 0 10px 3px rgba(0,0,0, 0.05);
+}
+.arrow {
+  visibility: hidden;
+  transition: transform 0.1s;
 }
 
-.sort-results .table-reversed li.current-sort::after {
-  content: '\2193';
+.current-sort .arrow {
+  visibility: visible;
 }
+
+.table-reversed .current-sort .arrow {
+  transform: rotate(180deg);
+}
+
+
 </style>
 <script>
 export default {
