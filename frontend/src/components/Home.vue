6e7162196538b177764c6fb4dc3611ea1a3c851c
Fletcher Dick <fdi19@uclive.ac.nz>
2021-05-15 10:39:28 +1200

    merge, trying to change task id

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -2,10 +2,13 @@
   <div class="container pt-4">
     <h1>Home Page</h1>
     <p>Beautiful, ain't it?</p>
+    <inventory-item-entry business-id="2"/>
   </div>
 </template>
 <script>
+import InventoryItemEntry from "@/components/InventoryItemEntry";
 export default {
-  name: "home"
+  name: "home",
+  components: {InventoryItemEntry}
 }
 </script>
