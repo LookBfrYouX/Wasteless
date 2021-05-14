8b27cf26c4885839d382a664c25ff9e494d06a63
Maximilian Birzer <mbi47@uclive.ac.nz>
2021-05-14 16:39:58 +1200

    S302T300-56 #time 5m ?document Create Inventory Post Controller Endpoint - Added javadoc to controller advice class.

--- backend/src/main/java/com/navbara_pigeons/wasteless/controller/ControllerExceptionHandler.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/controller/ControllerExceptionHandler.java
@@ -6,27 +6,45 @@ import com.navbara_pigeons.wasteless.exception.UserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpClientErrorException;
 
+/**
+ * This class handles all custom controller exceptions and returns the appropriate response entity for each.
+ * Error logging for controllers is also handled in this class.
+ * It does NOT handle Spring Security exceptions however.
+ */
 @ControllerAdvice
 @Slf4j
 public class ControllerExceptionHandler {
 
+    /**
+     * This is the exception handler for InsufficientPrivilegesExceptions.
+     * @param exc The thrown exception
+     * @return ResponseEntity with the exception message
+     */
     @ExceptionHandler(InsufficientPrivilegesException.class)
     public ResponseEntity<String> handleConflict(InsufficientPrivilegesException exc) {
         log.error("UNAUTHORISED ACTION: 403 - " + exc.getMessage());
         return new ResponseEntity<>(exc.getMessage(), HttpStatus.valueOf(403));
     }
 
+    /**
+     * This is the exception handler for BusinessNotFoundExceptions.
+     * @param exc The thrown exception
+     * @return ResponseEntity with the exception message
+     */
     @ExceptionHandler(BusinessNotFoundException.class)
     public ResponseEntity<String> handleBusinessNotFound(BusinessNotFoundException exc) {
         log.error("BUSINESS NOT FOUND: 406 - " + exc.getMessage());
         return new ResponseEntity<>(exc.getMessage(), HttpStatus.valueOf(406));
     }
 
+    /**
+     * This is the exception handler for UserNotFoundExceptions.
+     * @param exc The thrown exception
+     * @return ResponseEntity with the exception message
+     */
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleUserNotFound(UserNotFoundException exc) {
         log.error("USER NOT FOUND: 406 - " + exc.getMessage());
