ccd8f41547a31ce6e31b0385d17c52ed525bdd52
Fletcher Dick <fdi19@uclive.ac.nz>
2021-05-15 14:17:58 +1200

    S302T300-54 #time 2m Removed redundant code left from code review ?fix

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -2,13 +2,10 @@
   <div class="container pt-4">
     <h1>Home Page</h1>
     <p>Beautiful, ain't it?</p>
-    <inventory-item-entry business-id="2"/>
   </div>
 </template>
 <script>
-import InventoryItemEntry from "@/components/InventoryItemEntry";
 export default {
-  name: "home",
-  components: {InventoryItemEntry}
+  name: "home"
 }
 </script>
