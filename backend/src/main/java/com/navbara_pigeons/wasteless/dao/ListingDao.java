39c2d500fc8af263920f5afaec36d7718a2c870e
Alec Fox <amf133@uclive.ac.nz>
2021-05-16 23:03:24 +1200

    S302T300-81 #time 1m small fix ?fix - Missed this, oops

--- backend/src/main/java/com/navbara_pigeons/wasteless/dao/ListingDao.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/dao/ListingDao.java
@@ -1,7 +1,6 @@
 package com.navbara_pigeons.wasteless.dao;
 
 import com.navbara_pigeons.wasteless.entity.Listing;
-import com.navbara_pigeons.wasteless.exception.ListingNotFoundException;
 
 public interface ListingDao {
 
