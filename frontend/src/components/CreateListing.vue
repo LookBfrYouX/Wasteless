943d1bc7aa9fe74696bc346169dbf07b763d636c
Haruka Ichinose <hic21@uclive.ac.nz>
2021-05-17 17:51:07 +1200

    S302T300-76 #time 1h 15m ?implement ?pair[amf133, hic21] Implemented max quantity validation on selected product.

--- frontend/src/components/CreateListing.vue
+++ frontend/src/components/CreateListing.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="container">
-    <form 
-        method="POST"
-
+    <form
+        v-on:submit.prevent="addListing"
         >
       <div class="row">
         <div class="form-group required col-md-6">
@@ -10,12 +9,18 @@
           <select
               id="product"
               class="form-control"
-              required >
-            <option disabled selected value> -- Select product to list -- </option>
-            <option v-for="product in productOptions"
-                    :key="product.id"
-                    v-bind:value="productToList">
-              {{ product.name }}
+              required
+              v-model="selectedInventoryItem"
+              placeholder="Select">
+            <option disabled
+                    :value="null"
+                    selected> -- Select product to list -- </option>
+            <option v-for="inventoryItem in inventory"
+                    :key="inventoryItem.id"
+                    v-bind:value="inventoryItem"
+                    placeholder="Select a product"
+                    >
+              {{ inventoryItem.product.name }}
             </option>
           </select>
         </div>
@@ -23,11 +28,13 @@
           <label for="quantity">Quantity</label>
           <input
               id="quantity"
-              v-model="numberToList"
+              v-model="quantity"
+              :max="maxQuantity"
               class="form-control"
               name="quantity"
               required
-              type="text">
+              type="number"
+              min="1">
         </div>
       </div>
 
@@ -41,59 +48,183 @@
 
 <script>
 
-const {Api} = require("./../Api.js");
+// const {Api} = require("./../Api.js");
 
 export default {
   name: "CreateListing",
 
   data() {
     return {
-      productOptions: null,
-      productToList: null,
-      numberToList: 0,
-      productErrorMessage: null,
-      quantityErrorMessage: null,
-      errorMessage: "",
+      inventory: null,
+      selectedInventoryItem: null,
+      quantity: 0,
+      maxQuantity: 0,
     }
   },
 
-  props: {
-    businessId: {
-      required: true,
-      type: Number
-    }
-  },
+  // props: {
+  //   businessId: {
+  //     required: true,
+  //     type: Number
+  //   }
+  // },
 
   beforeMount: async function() {
     await this.getAvailableInventoryItem();
   },
 
+  watch: {
+    selectedInventoryItem: function (selectedInventoryItem) {
+      this.maxQuantity = selectedInventoryItem.quantityRemaining;
+    }
+  },
+
   methods: {
 
     async getInventory() {
-      await Api.getBusinessInventory(1)
-      .catch(err => this.apiErrorMessage = err.userFacingErrorMessage);
+      const inventory =
+          [
+            {
+              "id": 101,
+              "product": {
+                "id": 1,
+                "name": "Watties Baked Beans - 420g can",
+                "description": "Baked Beans as they should be.",
+                "manufacturer": "Heinz Wattie's Limited",
+                "recommendedRetailPrice": 2.2,
+                "created": "2021-05-17T04:30:33.616Z",
+                "images": [
+                  {
+                    "id": 1234,
+                    "filename": "/media/images/23987192387509-123908794328.png",
+                    "thumbnailFilename": "/media/images/23987192387509-123908794328_thumbnail.png"
+                  }
+                ]
+              },
+              "quantity": 4,
+              "pricePerItem": 6.5,
+              "totalPrice": 21.99,
+              "manufactured": "2021-05-17",
+              "sellBy": "2021-05-17",
+              "bestBefore": "2021-05-17",
+              "expires": "2021-05-17"
+            },
+            {
+              "id": 102,
+              "product": {
+                "id": 2,
+                "name": "Other product",
+                "description": "Baked Beans as they should be.",
+                "manufacturer": "Heinz Wattie's Limited",
+                "recommendedRetailPrice": 2.2,
+                "created": "2021-05-17T04:30:33.616Z",
+                "images": [
+                  {
+                    "id": 1234,
+                    "filename": "/media/images/23987192387509-123908794328.png",
+                    "thumbnailFilename": "/media/images/23987192387509-123908794328_thumbnail.png"
+                  }
+                ]
+              },
+              "quantity": 4,
+              "pricePerItem": 6.5,
+              "totalPrice": 21.99,
+              "manufactured": "2021-05-17",
+              "sellBy": "2021-05-17",
+              "bestBefore": "2021-05-17",
+              "expires": "2021-05-17"
+            },
+            {
+              "id": 103,
+              "product": {
+                "id": 3,
+                "name": "Another product",
+                "description": "Baked Beans as they should be.",
+                "manufacturer": "Heinz Wattie's Limited",
+                "recommendedRetailPrice": 2.2,
+                "created": "2021-05-17T04:30:33.616Z",
+                "images": [
+                  {
+                    "id": 1234,
+                    "filename": "/media/images/23987192387509-123908794328.png",
+                    "thumbnailFilename": "/media/images/23987192387509-123908794328_thumbnail.png"
+                  }
+                ]
+              },
+              "quantity": 4,
+              "pricePerItem": 6.5,
+              "totalPrice": 21.99,
+              "manufactured": "2021-05-17",
+              "sellBy": "2021-05-17",
+              "bestBefore": "2021-05-17",
+              "expires": "2021-05-17"
+            }
+          ]
+      return inventory;
+      // await Api.getBusinessInventory(1)
+      // .catch(err => this.apiErrorMessage = err.userFacingErrorMessage);
     },
 
     async getListings() {
-      await Api.getBusinessListings(1)
-      .catch(err => this.apiErrorMessage = err.userFacingErrorMessage);
+      const listings =
+          [
+            {
+              "id": 57,
+              "inventoryItem": {
+                "id": 101,
+                "product": {
+                  "id": 1,
+                  "name": "Watties Baked Beans - 420g can",
+                  "description": "Baked Beans as they should be.",
+                  "manufacturer": "Heinz Wattie's Limited",
+                  "recommendedRetailPrice": 2.2,
+                  "created": "2021-05-17T04:49:48.728Z",
+                  "images": [
+                    {
+                      "id": 1234,
+                      "filename": "/media/images/23987192387509-123908794328.png",
+                      "thumbnailFilename": "/media/images/23987192387509-123908794328_thumbnail.png"
+                    }
+                  ]
+                },
+                "quantity": 4,
+                "pricePerItem": 6.5,
+                "totalPrice": 21.99,
+                "manufactured": "2021-05-17",
+                "sellBy": "2021-05-17",
+                "bestBefore": "2021-05-17",
+                "expires": "2021-05-17"
+              },
+              "quantity": 3,
+              "price": 17.99,
+              "moreInfo": "Seller may be willing to consider near offers",
+              "created": "2021-07-14T11:44:00Z",
+              "closes": "2021-07-21T23:59:00Z"
+            }
+          ]
+      // await Api.getBusinessListings(1)
+      // .catch(err => this.apiErrorMessage = err.userFacingErrorMessage);
+      return listings;
     },
 
     async getAvailableInventoryItem() {
-      var inventoryItems = await this.getInventory();
-      inventoryItems = [].map(inventoryItem => {
+      let inventory = await this.getInventory();
+      inventory.map(inventoryItem => {
         inventoryItem.quantityRemaining = inventoryItem.quantity;
         return inventoryItem;
       });
       const listings = await this.getListings();
       listings.forEach(listing => {
-        const inventoryItem = inventoryItems.find(({ id }) => id === listing.inventoryItem.id);
+        const inventoryItem = inventory.find(({ id }) => id === listing.inventoryItem.id);
         inventoryItem.quantityRemaining -= listing.quantity;
       });
-      this.productOptions = inventoryItems;
+      this.inventory = inventory;
 
+    },
+    addListing() {
+      alert("Not yet implemented");
     }
+    
   }
 }
 </script>
