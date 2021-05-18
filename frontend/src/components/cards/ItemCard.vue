88cfb8051e0bfccde6eacbc619feabac9d633f47
Haruka Ichinose <hic21@uclive.ac.nz>
2021-05-18 18:01:08 +1200

    S302T300-171 #time 10m ?implement Disabled text truncate when the card is expanded.

--- frontend/src/components/cards/ItemCard.vue
+++ frontend/src/components/cards/ItemCard.vue
@@ -1,12 +1,16 @@
 <template>
   <!-- this component produces the item card component from the given props -->
-  <div class="container card item-card" data-toggle="collapse" v-bind:data-target="'#' + idPrefix + item.id" @click="console.log('clicked')"> <!-- TODO add onclick handler and method when components and routes are ready -->
+  <div class="container card item-card"
+       data-toggle="collapse"
+       v-bind:data-target="'#' + idPrefix + item.id"
+       @click="toggleCollapse()"> <!-- TODO add onclick handler and method when components and routes are ready -->
     <div class="row card-body">
       <div class="col-2 pr-0">
         <img class="img-fluid item-image" src="./../../../assets/images/login.jpg" alt="Image" />
       </div>
       <div class="col-10">
-        <h1 class="card-title text-truncate">{{ item.product.name }}</h1>
+        <h1 class="card-title"
+            v-bind:class="{'text-truncate': !isExpanded}">{{ item.product.name }}</h1>
         <div class="d-flex flex-wrap justify-content-between">
           <h4 class="pr-2">{{ item.quantity }} <small>units in stock</small></h4>
           <h4 class="pr-2">
@@ -20,7 +24,8 @@
           <small>{{ metaValues[0].key }}:</small> <span class="text-nowrap">{{ metaValues[0].value}}</span>
         </h4>
 
-        <div class="collapse" v-bind:id="idPrefix + item.id">
+        <div class="collapse"
+             v-bind:id="idPrefix + item.id">
           <div v-for="(meta, index) in metaValues"
                v-bind:key="index">
             <h4 v-if="index !== 0"
@@ -34,35 +39,28 @@
       </div>
     </div>
 
-<!--    <div class="row collapse" >-->
-<!--      <div class="col-10 offset-2">-->
-<!--        <div class="d-flex flex-wrap align-items-baseline w-100">-->
-<!--          <h3 class="mb-0 mr-3">{{ item.totalPrice }}</h3>-->
-<!--          <p class="mb-0">{{ item.quantity }} units @ ${{ item.pricePerItem }} each</p>-->
-<!--        </div>-->
-<!--        <div class="item-description w-100">-->
-<!--          <div class="py-2">{{ item.product.description }}</div>-->
-<!--        </div>-->
-<!--        <div v-for="(meta, index) in metaValues"-->
-<!--             v-if="index !== 0"-->
-<!--             v-bind:key="index"-->
-<!--             class="col-sm-6 col-md-3 item-meta">-->
-<!--          <h4>{{ meta.key }}:</h4> {{ meta.value }}-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
   </div>
 </template>
 
 <script>
 export default {
   name: "ItemCard",
+  data() {
+    return {
+      isExpanded: false,
+    }
+  },
   props: {
     // The item information to be displayed. (See API spec for details)
     item: Object,
     idPrefix: {
       type: String,
       default: "inventory-item-"
+    }
+  },
+  methods: {
+    toggleCollapse() {
+      this.isExpanded = !this.isExpanded;
     }
   },
   computed: {
