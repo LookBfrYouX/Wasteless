0396c5398d0660f028003e02d1b5eb7edc97132c
Alec Fox <amf133@uclive.ac.nz>
2021-05-24 19:38:52 +1200

    S302T300-81 #time 1m listing controller now returns object ?fix

--- backend/src/main/java/com/navbara_pigeons/wasteless/controller/ListingController.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/controller/ListingController.java
@@ -7,6 +7,7 @@ import com.navbara_pigeons.wasteless.exception.ListingValidationException;
 import com.navbara_pigeons.wasteless.exception.*;
 import com.navbara_pigeons.wasteless.service.ListingService;
 import lombok.extern.slf4j.Slf4j;
+import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -40,13 +41,15 @@ public class ListingController {
    * @return Returns the newly created listing id and 201 status code in a ResponseEntity
    */
   @PostMapping("/businesses/{businessId}/listings")
-  public ResponseEntity<Long> addListing(@PathVariable long businessId,
+  public ResponseEntity<JSONObject> addListing(@PathVariable long businessId,
       @RequestBody CreateListingDto listingDto)
       throws UserNotFoundException, BusinessNotFoundException, InsufficientPrivilegesException, ListingValidationException, InventoryItemNotFoundException {
 
     Long listingId = listingService.addListing(businessId, listingDto.getInventoryItemId(), new Listing(listingDto));
     log.info("LISTING CREATED SUCCESSFULLY: " + listingId + " FOR BUSINESS " + businessId);
-    return new ResponseEntity<>(listingId, HttpStatus.CREATED);
+    JSONObject response = new JSONObject();
+    response.appendField("listingId", listingId);
+    return new ResponseEntity<>(response, HttpStatus.CREATED);
   }
 
   /**
