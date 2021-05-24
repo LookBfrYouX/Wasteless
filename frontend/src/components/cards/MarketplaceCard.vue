c9e6f28ca6193769ee9e6d04f3817d156511d768
Fletcher Dick <fdi19@uclive.ac.nz>
2021-05-24 15:44:49 +1200

    S302T300-167 #time 55m Created card component for marketplace ?implement - Added in mock card data from spec - Created a filtering method to sort all card into their section types (e.g. For Sale, Exchange, etc.)

---
+++ frontend/src/components/cards/MarketplaceCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <!-- this component produces the marketplace card component from the given props -->
+  <div class="container card item-card p-3">
+    <div class="d-sm-flex align-items-center">
+      <a v-on:click="$router.push({name: 'profile', params: {userId: $props.card.creator.id}})"
+      href="javascript:void(0)">
+        <img alt="User is acting as self"
+             class="nav-picture rounded-circle border"
+             src="./../../../assets/images/default-user-thumbnail.svg"
+        >
+        {{ $props.card.creator.firstName }} {{ $props.card.creator.lastName }}
+      </a>
+      <div class="ml-auto d-flex flex-sm-column text-muted">
+        <span class="mb-sm-1 mr-3">
+          Created {{ $helper.isoToDateString($props.card.created) }}
+        </span>
+        <span>
+          Expires {{ $helper.isoToDateString($props.card.displayPeriodEnd) }}
+        </span>
+      </div>
+    </div>
+    <div class="text-muted mt-2">
+      {{
+        [$props.card.creator.homeAddress.suburb, $props.card.creator.homeAddress.city].join(', ')
+      }}
+    </div>
+    <div class="mt-3">
+      <h2>{{ $props.card.title }}</h2>
+      {{ $props.card.description }}
+    </div>
+    <div class="mt-2">
+      {{ keywords }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MarketplaceCard",
+  props: {
+    // The card information to be displayed. (See API spec for more details)
+    card: Object
+  },
+  data() {
+    return {
+      keywords: []
+    };
+  },
+  mounted() {
+    this.keywords = this.$props.card.keywords.map((kw) => {
+      return kw.name
+    });
+  }
+}
+</script>
+
+<style scoped>
+
+a {
+  color: black;
+}
+
+</style>
--- frontend/src/views/Marketplace.vue
+++ frontend/src/views/Marketplace.vue
@@ -1,21 +1,112 @@
 <template>
   <div class="w-100">
     <h2>Community Marketplace</h2>
+
+    <div
+      v-for="card in forSaleCards"
+      v-bind:key="card.id.toString() + card.created"
+    >
+      <MarketplaceCard :card="card"/>
+    </div>
   </div>
 </template>
 
 <script>
+import MarketplaceCard from "@/components/cards/MarketplaceCard";
 
+const mockCards = [
+  {
+    "id": 500,
+    "creator": {
+      "id": 100,
+      "firstName": "John",
+      "lastName": "Smith",
+      "middleName": "Hector",
+      "nickname": "Jonny",
+      "bio": "Likes long walks on the beach",
+      "email": "johnsmith99@gmail.com",
+      "dateOfBirth": "1999-04-27",
+      "phoneNumber": "+64 3 555 0129",
+      "homeAddress": {
+        "streetNumber": "3/24",
+        "streetName": "Ilam Road",
+        "suburb": "Upper Riccarton",
+        "city": "Christchurch",
+        "region": "Canterbury",
+        "country": "New Zealand",
+        "postcode": "90210"
+      },
+      "created": "2020-07-14T14:32:00Z",
+      "role": "user",
+      "businessesAdministered": [
+        {
+          "id": 100,
+          "administrators": [
+            "string"
+          ],
+          "primaryAdministratorId": 20,
+          "name": "Lumbridge General Store",
+          "description": "A one-stop shop for all your adventuring needs",
+          "address": {
+            "streetNumber": "3/24",
+            "streetName": "Ilam Road",
+            "suburb": "Upper Riccarton",
+            "city": "Christchurch",
+            "region": "Canterbury",
+            "country": "New Zealand",
+            "postcode": "90210"
+          },
+          "businessType": "Accommodation and Food Services",
+          "created": "2020-07-14T14:52:00Z"
+        }
+      ]
+    },
+    "section": "ForSale",
+    "created": "2021-07-15T05:10:00Z",
+    "displayPeriodEnd": "2021-07-29T05:10:00Z",
+    "title": "1982 Lada Samara",
+    "description": "Beige, suitable for a hen house. Fair condition. Some rust. As is, where is. Will swap for budgerigar.",
+    "keywords": [
+      {
+        "id": 600,
+        "name": "Vehicle",
+        "created": "2021-07-15T05:10:00Z"
+      }
+    ]
+  }
+];
 
 export default {
-  components: {},
+  components: {MarketplaceCard},
   props: {},
   data() {
-    return {};
+    return {
+      forSaleCards: [],
+      wantedCards: [],
+      exchangeCards: []
+    };
   },
-  beforeMount: async function () {
+  mounted() {
+    this.getCards();
   },
-  methods: {},
-  watch: {}
+  methods: {
+    getCards() {
+      // This method will be used to get the cards from the API, for now using mocked cards
+      const recvCards = mockCards;
+      this.sortCards(recvCards);
+    },
+    sortCards(cards) {
+      // Sort the received card into their own sections
+      this.forSaleCards = cards.filter((card) => {
+        return card.section === 'ForSale';
+      });
+      this.wantedCards = cards.filter((card) => {
+        return card.section === 'Wanted';
+      });
+      this.exchangeCards = cards.filter((card) => {
+        return card.section === 'Exchange';
+      });
+    }
+  }
 }
 </script>
