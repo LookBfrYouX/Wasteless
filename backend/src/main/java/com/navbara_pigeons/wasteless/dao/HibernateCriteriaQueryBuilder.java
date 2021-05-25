c50d566cbc19fe56bd56385f5d27527476b8cce8
Maximilian Birzer <mbi47@uclive.ac.nz>
2021-05-25 17:16:07 +1200

    S302T300-187 #time 30m ?fix Sprint 2 fixes - Search now works with quotation marks for exact matches.

--- backend/src/main/java/com/navbara_pigeons/wasteless/dao/HibernateCriteriaQueryBuilder.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/dao/HibernateCriteriaQueryBuilder.java
@@ -49,13 +49,14 @@ public class HibernateCriteriaQueryBuilder {
     // -- currently just using spaces
 
     List<String> tokens = new ArrayList<>();
-    Matcher matcher = Pattern.compile("([^\"]\\S*|\"[^>]++\")\\s*").matcher(searchQuery);
+    Matcher matcher = Pattern.compile("(\"[^\"]+\")|([^\"\\s]+)").matcher(searchQuery);
     while (matcher.find()) {
-      tokens.add(matcher.group(1));
+      tokens.add(matcher.group(0));
     }
 
     for (int i = 0; i < tokens.size(); i++) {
       String currentToken = tokens.get(i);
+      System.out.println(currentToken);
       if (currentToken.toUpperCase().matches("AND")) {
         // AND is default join for predicates so this is empty
       } else if (currentToken.toUpperCase().matches("OR")) {
