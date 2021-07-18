2c978605d1275dde185701abf85f2b44e62c77ba
Alec Fox <amf133@uclive.ac.nz>
2021-07-18 22:13:49 +1200

    S302T300-238 #time 45m improved image script ?implement ?pair[amf133, rog19] - Pair programmed with Rio, writing dummy data image script

--- dummyData/imageScript.js
+++ dummyData/imageScript.js
@@ -1,5 +1,14 @@
-import axios from 'axios';
-import {Api} from '../frontend/src/Api';
+const FormData = require("form-data");
+const axios = require('axios');
+const SERVER_URL = "http://localhost:9499";
+const fs = require("fs");
+let cookie = "";
+
+const instance = axios.create({
+  baseURL: SERVER_URL,
+  timeout: 10000,
+  withCredentials: true
+});
 
 /**
  * Queries the picsum.photos API to retrieve and download a random image
@@ -8,19 +17,43 @@ async function getImage() {
   return await axios
   .get(`https://picsum.photos/200`)
   .then((response) => {
-    return response;
+    return response.data;
   })
 }
 
-await Api.login({email: "amf133@uclive.ac.nz", password: "fun123"});
+(async () => {
+  await instance.post("/login",
+      {email: "amf133@uclive.ac.nz", password: "fun123"}).then((response) => {
+    cookie = response.headers["set-cookie"][0].split(";")[0];
+  });
+  let image = await getImage();
+  fs.writeFileSync("./a.jpg", image);
 
-for (let i = 0; i < 5000; i++) {
-  // download random image
-  let image = getImage()
 
-  // upload random image
-  Api.uploadProductImage(image, i%1000, i);
+  const formData = new FormData();
+  formData.append("image", fs.createReadStream("./a.jpg"));
 
-  // delete random image somehow
-  console.log("........");
-}
+  console.log(formData);
+
+  instance.post(
+      `/businesses/${1}/products/${1}/images`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          cookie
+        }
+      }).then((response) => {
+    console.log(response)
+  }).catch((err)=>{console.log(err)})
+
+
+})();
+
+// for (let i = 0; i < 5000; i++) {
+//   // download random image
+//   let image = await getImage();
+//
+//   // upload random image
+//   await Api.uploadProductImage(image, i%1000, i);
+//
+//   // delete random image somehow
+// }
