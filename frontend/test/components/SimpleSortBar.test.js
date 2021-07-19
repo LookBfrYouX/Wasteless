7ee79a7635c0d987f35660971d378092047c8244
Maximilian Birzer <mbi47@uclive.ac.nz>
2021-07-19 19:37:56 +1200

    S302T300-222 #time 40m Fixed jest and vuetify test setup ?test ?pair[rog19, mbi47] - Got basic jest vuetify test working.

--- frontend/test/components/SimpleSortBar.test.js
+++ frontend/test/components/SimpleSortBar.test.js
@@ -1,15 +1,23 @@
 import {createLocalVue, mount} from "@vue/test-utils"
 import SimpleSortBar from "@/components/SimpleSortBar";
-import vuetify from "@/plugins/vuetify";
+
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify);
+let vuetify = new Vuetify();
+
+let wrapper;
+
+afterEach(() => wrapper.destroy());
 
 describe("SimpleSortBar test", () => {
-    const localVue = createLocalVue();
-    localVue.use(vuetify)
     test("just testing for now", () => {
-        const wrapper = mount(SimpleSortBar, {
+        wrapper = mount(SimpleSortBar, {
+            vuetify,
             propsData: {
                 items: [{key: 'name', value: 'name-desc'}],
-            }
+            },
         });
 
         wrapper.vm.sortChange('name');
