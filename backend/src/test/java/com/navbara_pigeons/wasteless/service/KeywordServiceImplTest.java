e2fa75b079f56d2c7fb08d97c11420e2f39dc28f
Haruka Ichinose <hic21@uclive.ac.nz>
2021-07-20 16:01:06 +1200

    S302T300-229 #time 5m ?fix ?pair[mbi47, hic21] Fixed import to make JUnit test run.

--- backend/src/test/java/com/navbara_pigeons/wasteless/service/KeywordServiceImplTest.java
+++ backend/src/test/java/com/navbara_pigeons/wasteless/service/KeywordServiceImplTest.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
 public class KeywordServiceImplTest extends ServiceTestProvider {
