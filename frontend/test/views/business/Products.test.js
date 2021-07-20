9da9de96c993fc72aa3168e1ef0e0ce79c8e332b
Maximilian Birzer <mbi47@uclive.ac.nz>
2021-07-20 15:23:11 +1200

    S302T300-222 #time 35m Started implementing tests for Products ?test - Added and tested imports and test setup

---
+++ frontend/test/views/business/Products.test.js
@@ -0,0 +1,38 @@
+import Products from "@/views/business/Products";
+import {mount} from "@vue/test-utils";
+import {globalStateMocks} from "#/testHelper";
+import {Api} from "@/Api";
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify);
+let vuetify = new Vuetify();
+
+jest.mock("@/Api");
+
+let wrapper;
+
+let products = [
+    {id: 1, name: "Product 1", description: "Description 1", manufacturer: "Manufacturer 1", recommendedRetailPrice: 4.67, created: "2020-07-14T14:32:00+12:00", primaryProductImage: null, images: []},
+    {id: 2, name: "Product 2", description: "Description 2", manufacturer: "Manufacturer 2", recommendedRetailPrice: 3.0, created: "2020-07-14T14:32:00+12:00", primaryProductImage: null, images: []},
+    {id: 3, name: "Product 3", description: "Description 3", manufacturer: "Manufacturer 3", recommendedRetailPrice: 3.3, created: "2020-07-14T14:32:00+12:00", primaryProductImage: null, images: []}
+]
+
+beforeEach(() => {
+    wrapper = mount(Products, {
+        vuetify,
+        mocks: globalStateMocks(),
+        stubs: ["error-modal", "router-link"], // Add the name of the business listings item component to here
+        propsData: {
+            businessId: 1
+        }
+    });
+});
+
+afterEach(() => wrapper.destroy());
+
+describe("Product API handling", () => {
+    test("Does it get to here", () => {
+        console.log("Yes it does!");
+    });
+});
