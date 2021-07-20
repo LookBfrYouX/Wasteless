a17078ed219d42f5f2a06e2e88914689d0462a82
Alec Fox <amf133@uclive.ac.nz>
2021-07-20 22:59:55 +1200

    S302T300-238 #time 1h 30m almost finished the imageScript.js file! ?implement - Managed to successfully download a random image from picsum - Struggling to upload the random image to our API, might have to call for backup!

--- dummyData/imageScript.js
+++ dummyData/imageScript.js
@@ -1,10 +1,11 @@
 const FormData = require("form-data");
-const axios = require('axios');
+const Axios = require('axios');
 const SERVER_URL = "http://localhost:9499";
-const fs = require("fs");
+const Fs = require("fs");
 let cookie = "";
+const Path = require('path')
 
-const instance = axios.create({
+const instance = Axios.create({
   baseURL: SERVER_URL,
   timeout: 10000,
   withCredentials: true
@@ -13,39 +14,49 @@ const instance = axios.create({
 /**
  * Queries the picsum.photos API to retrieve and download a random image
  */
-async function getImage() {
-  return await axios
-  .get(`https://picsum.photos/200`)
-  .then((response) => {
-    return response.data;
+async function downloadImage() {
+  const url = 'https://picsum.photos/200'
+  const path = Path.resolve(__dirname, '', 'a.jpg')
+  const writer = Fs.createWriteStream(path)
+
+  const response = await Axios({
+    url,
+    method: 'GET',
+    responseType: 'stream'
   })
+  response.data.pipe(writer)
 }
 
-(async () => {
+async function login() {
   await instance.post("/login",
       {email: "amf133@uclive.ac.nz", password: "fun123"}).then((response) => {
     cookie = response.headers["set-cookie"][0].split(";")[0];
   });
-  let image = await getImage();
-  fs.writeFileSync("./a.jpg", image);
+}
 
+async function uploadImage() {
+  let filePath = __dirname + "/a.jpg";
 
-  const formData = new FormData();
-  formData.append("image", fs.createReadStream("./a.jpg"));
+  Fs.readFile(filePath, (error ,imageData) => {
+    console.log(imageData.toString('binary'));
+    Axios.post(
+        `http://localhost:9499/businesses/${1}/products/${1}/images`, imageData.toString('binary'), {
+          headers: {
+            'Content-Type': 'multipart/form-data; boundary=<calculated when request is sent>',
+            cookie
+          }
+        }).then((response) => {
+      console.log(response)
+    }).catch((err) => {
+      console.log(err)
+    })
+  });
+}
 
-  console.log(formData);
-
-  instance.post(
-      `/businesses/${1}/products/${1}/images`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          cookie
-        }
-      }).then((response) => {
-    console.log(response)
-  }).catch((err)=>{console.log(err)})
-
-
+(async () => {
+  await login();
+  await downloadImage();
+  await uploadImage();
 })();
 
 // for (let i = 0; i < 5000; i++) {
