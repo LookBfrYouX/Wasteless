214e7fff41ad2ce7bcaa0149e1194b3f70c65dda
Alec Fox <amf133@uclive.ac.nz>
2021-07-21 12:56:46 +1200

    S302T300-238 #time 40m image upload script complete and manual tested ?implement - Problem is that the script takes a long time to run (35-40 minutes) but this was expected and changing the number of inserts is very easy

--- dummyData/imageScript.js
+++ dummyData/imageScript.js
@@ -1,15 +1,15 @@
-const FormData = require("form-data");
 const Axios = require('axios');
-const SERVER_URL = "http://localhost:9499";
+const FormData = require("form-data");
 const Fs = require("fs");
-let cookie = "";
 const Path = require('path')
+const SERVER_URL = "http://localhost:9499";
+let cookie = "";
 
 /**
  * Queries the picsum.photos API to retrieve and download a random image
  */
 async function downloadImage() {
-  const url = 'https://picsum.photos/200'
+  const url = 'https://picsum.photos/50'
   const path = Path.resolve(__dirname, '', 'a.jpg')
   const writer = Fs.createWriteStream(path)
 
@@ -22,11 +22,11 @@ async function downloadImage() {
 }
 
 /**
- * Logs into the amf133 account (which has admin rights)
+ * Logs into the admin account to upload an image for any business
  */
 async function login() {
   await Axios.post(`${SERVER_URL}/login`,
-      {email: "amf133@uclive.ac.nz", password: "fun123"}).then((response) => {
+      {email: "admin@wasteless.co.nz", password: "admin"}).then((response) => {
     cookie = response.headers["set-cookie"][0].split(";")[0];
   });
 }
@@ -48,21 +48,22 @@ async function uploadImage(businessId, productId) {
     },
     data: data
   };
-  Axios(config);
+  Axios(config).then(function() {
+    console.log("Success");
+  }).catch(function(error) {
+    console.log(error);
+  });
 }
 
+/**
+ * Main script to download and upload images
+ */
 (async () => {
   await login();
-  await downloadImage();
-  await uploadImage(1, 1);
+  for (let i = 1; i < 5000; i++) {
+    await downloadImage();
+    await uploadImage(i%1000, i);
+  }
 })();
 
-// for (let i = 0; i < 5000; i++) {
-//   // download random image
-//   let image = await getImage();
-//
-//   // upload random image
-//   await Api.uploadProductImage(image, i%1000, i);
-//
-//   // delete random image somehow
-// }
+
