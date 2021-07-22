d0fa4d7b4954e90053af551fd6a172e9c151e612
Haruka Ichinose <hic21@uclive.ac.nz>
2021-07-22 18:20:16 +1200

    S302T300-229 #time 15m ?implement ?pair[nbt25, hic21] Added Hibernate Validation dependency in gradle and constrained keyword length in CreateKeywordDto.

--- backend/src/main/java/com/navbara_pigeons/wasteless/dto/CreateKeywordDto.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/dto/CreateKeywordDto.java
@@ -1,11 +1,15 @@
 package com.navbara_pigeons.wasteless.dto;
 
 import com.navbara_pigeons.wasteless.entity.Keyword;
+import javax.validation.constraints.NotBlank;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class CreateKeywordDto {
 
+  @NotBlank(message = "Keyword is Required")
+  @Length(max = 50, message = "Keyword has to be less than or equal to 50 Characters")
   private String name;
 
   public CreateKeywordDto(Keyword keyword) {
