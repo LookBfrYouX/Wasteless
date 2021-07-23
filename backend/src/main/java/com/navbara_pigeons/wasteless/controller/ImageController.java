09054b607a560932949605f9fd171fa187b88f69
amf133 <amf133@uclive.ac.nz>
2021-07-23 13:05:49 +1200

    S302T300-238 #time 40m bug fixes ?fix - Fixed bug in image controller (@RestController was missing) - Fixed bug in user search (need data.results now as backend has changed) - Moved imageScript.js to frontend so we dont have to run npm install in the dummydata folder

--- backend/src/main/java/com/navbara_pigeons/wasteless/controller/ImageController.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/controller/ImageController.java
@@ -12,16 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 /** @author Alec Fox, Fletcher Dick, Haruka Ichinose */
 @Slf4j
+@RestController
 public class ImageController {
 
   private final ImageService imageService;
