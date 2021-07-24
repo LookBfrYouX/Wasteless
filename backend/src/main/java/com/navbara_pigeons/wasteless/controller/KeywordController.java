b9c661e3cc1fcad5ecb46f05516f803471336e77
Maximilian Birzer <mbi47@uclive.ac.nz>
2021-07-25 10:29:42 +1200

    S302T300-237 #time 35m Added getKeywords mapping and did manual tests ?implement ?test - Manually tested using postman. - Added endpoint.

--- backend/src/main/java/com/navbara_pigeons/wasteless/controller/KeywordController.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/controller/KeywordController.java
@@ -1,10 +1,14 @@
 package com.navbara_pigeons.wasteless.controller;
 
+import com.navbara_pigeons.wasteless.dto.CreateKeywordDto;
+import com.navbara_pigeons.wasteless.entity.Keyword;
+import com.navbara_pigeons.wasteless.service.KeywordService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class KeywordController {
@@ -15,9 +19,10 @@ public class KeywordController {
         this.keywordService = keywordService;
     }
 
-    @GetMapping('/keywords/{id}')
-    public ResponseEntity getKeywords(@PathVariable Long id) {
-
+    @GetMapping("/keywords")
+    public ResponseEntity getKeywords(@RequestParam List<Long> ids) {
+        System.out.println(ids);
+        return new ResponseEntity(keywordService.getKeywords(ids), HttpStatus.valueOf(200));
     }
 
 }
