7017d785f51d154f5c7918a7d3051711a6973d5a
Maximilian Birzer <mbi47@uclive.ac.nz>
2021-07-25 11:16:47 +1200

    S302T300-237 #time 2m Pulled in branch with service implemented ?chore - Controller and service match

--- backend/src/main/java/com/navbara_pigeons/wasteless/controller/KeywordController.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/controller/KeywordController.java
@@ -1,14 +1,10 @@
 package com.navbara_pigeons.wasteless.controller;
 
-import com.navbara_pigeons.wasteless.dto.CreateKeywordDto;
-import com.navbara_pigeons.wasteless.entity.Keyword;
 import com.navbara_pigeons.wasteless.service.KeywordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class KeywordController {
