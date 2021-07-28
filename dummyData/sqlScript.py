38b24cc2a5eab029d3bd868e65eebb40c00d2bfa
Rio Ogino <rog19@uclive.ac.nz>
2021-07-28 13:37:15 +1200

    S302T300-268 #time 3m ?fix sql script country name mapping

--- dummyData/sqlScript.py
+++ dummyData/sqlScript.py
@@ -21,32 +21,32 @@ file = open("database.sql")
 # made by subtacting restcountries countries from sql output countries.
 # Saint Barthélemy: UTF encoding is horrible so just switch to france
 newFile.write("""
-UPDATE ADDRESS SET COUNTRY = 'Antarctica' WHERE COUNTRY = 'Antarctica (the territory South of 60 deg S)';
-UPDATE ADDRESS SET COUNTRY = 'Bolivia (Plurinational State of)' WHERE COUNTRY = 'Bolivia';
-UPDATE ADDRESS SET COUNTRY = 'Norway' WHERE COUNTRY = 'Bouvet Island (Bouvetoya)';
-UPDATE ADDRESS SET COUNTRY = 'British Indian Ocean Territory' WHERE COUNTRY = 'British Indian Ocean Territory (Chagos Archipelago)';
-UPDATE ADDRESS SET COUNTRY = 'Virgin Islands (British)' WHERE COUNTRY = 'British Virgin Islands';
-UPDATE ADDRESS SET COUNTRY = 'France' WHERE COUNTRY = 'French Southern Territories';
-UPDATE ADDRESS SET COUNTRY = 'Australia' WHERE COUNTRY = 'Heard Island and McDonald Islands';
-UPDATE ADDRESS SET COUNTRY = 'Italy' WHERE COUNTRY = 'Holy See (Vatican City State)';
-UPDATE ADDRESS SET COUNTRY = 'Iran (Islamic Republic of)' WHERE COUNTRY = 'Iran';
-UPDATE ADDRESS SET COUNTRY = 'Korea (Republic of)' WHERE COUNTRY = 'Korea';
-UPDATE ADDRESS SET COUNTRY = 'Kyrgyzstan' WHERE COUNTRY = 'Kyrgyz Republic';
-UPDATE ADDRESS SET COUNTRY = 'Libya' WHERE COUNTRY = 'Libyan Arab Jamahiriya';
-UPDATE ADDRESS SET COUNTRY = 'Macedonia (the former Yugoslav Republic of)' WHERE COUNTRY = 'Macedonia';
-UPDATE ADDRESS SET COUNTRY = 'Netherlands' WHERE COUNTRY = 'Netherlands Antilles';
-UPDATE ADDRESS SET COUNTRY = 'Palestine, State of' WHERE COUNTRY = 'Palestinian Territory';
-UPDATE ADDRESS SET COUNTRY = 'Pitcairn' WHERE COUNTRY = 'Pitcairn Islands';
-UPDATE ADDRESS SET COUNTRY = 'France' WHERE COUNTRY = 'Reunion';
-UPDATE ADDRESS SET COUNTRY = 'France' WHERE COUNTRY = 'Saint Barthelemy';
-UPDATE ADDRESS SET COUNTRY = 'Saint Helena, Ascension and Tristan da Cunha' WHERE COUNTRY = 'Saint Helena';
-UPDATE ADDRESS SET COUNTRY = 'Saint Martin (French part)' WHERE COUNTRY = 'Saint Martin';
-UPDATE ADDRESS SET COUNTRY = 'Slovakia' WHERE COUNTRY = 'Slovakia (Slovak Republic)';
-UPDATE ADDRESS SET COUNTRY = 'Svalbard and Jan Mayen' WHERE COUNTRY = 'Svalbard & Jan Mayen Islands';
-UPDATE ADDRESS SET COUNTRY = 'Tanzania, United Republic of' WHERE COUNTRY = 'Tanzania';
-UPDATE ADDRESS SET COUNTRY = 'United Kingdom of Great Britain and Northern Ireland' WHERE COUNTRY = 'United Kingdom';
-UPDATE ADDRESS SET COUNTRY = 'United States of America' WHERE COUNTRY = 'United States Minor Outlying Islands';
-UPDATE ADDRESS SET COUNTRY = 'Virgin Islands (U.S.)' WHERE COUNTRY = 'United States Virgin Islands';
-UPDATE ADDRESS SET COUNTRY = 'Venezuela (Bolivarian Republic of)' WHERE COUNTRY = 'Venezuela';
-UPDATE ADDRESS SET COUNTRY = 'Viet Nam' WHERE COUNTRY = 'Vietnam';""")
+UPDATE `address` SET COUNTRY = 'Antarctica' WHERE COUNTRY = 'Antarctica (the territory South of 60 deg S)';
+UPDATE `address` SET COUNTRY = 'Bolivia (Plurinational State of)' WHERE COUNTRY = 'Bolivia';
+UPDATE `address` SET COUNTRY = 'Norway' WHERE COUNTRY = 'Bouvet Island (Bouvetoya)';
+UPDATE `address` SET COUNTRY = 'British Indian Ocean Territory' WHERE COUNTRY = 'British Indian Ocean Territory (Chagos Archipelago)';
+UPDATE `address` SET COUNTRY = 'Virgin Islands (British)' WHERE COUNTRY = 'British Virgin Islands';
+UPDATE `address` SET COUNTRY = 'France' WHERE COUNTRY = 'French Southern Territories';
+UPDATE `address` SET COUNTRY = 'Australia' WHERE COUNTRY = 'Heard Island and McDonald Islands';
+UPDATE `address` SET COUNTRY = 'Italy' WHERE COUNTRY = 'Holy See (Vatican City State)';
+UPDATE `address` SET COUNTRY = 'Iran (Islamic Republic of)' WHERE COUNTRY = 'Iran';
+UPDATE `address` SET COUNTRY = 'Korea (Republic of)' WHERE COUNTRY = 'Korea';
+UPDATE `address` SET COUNTRY = 'Kyrgyzstan' WHERE COUNTRY = 'Kyrgyz Republic';
+UPDATE `address` SET COUNTRY = 'Libya' WHERE COUNTRY = 'Libyan Arab Jamahiriya';
+UPDATE `address` SET COUNTRY = 'Macedonia (the former Yugoslav Republic of)' WHERE COUNTRY = 'Macedonia';
+UPDATE `address` SET COUNTRY = 'Netherlands' WHERE COUNTRY = 'Netherlands Antilles';
+UPDATE `address` SET COUNTRY = 'Palestine, State of' WHERE COUNTRY = 'Palestinian Territory';
+UPDATE `address` SET COUNTRY = 'Pitcairn' WHERE COUNTRY = 'Pitcairn Islands';
+UPDATE `address` SET COUNTRY = 'France' WHERE COUNTRY = 'Reunion';
+UPDATE `address` SET COUNTRY = 'France' WHERE COUNTRY = 'Saint Barthelemy';
+UPDATE `address` SET COUNTRY = 'Saint Helena, Ascension and Tristan da Cunha' WHERE COUNTRY = 'Saint Helena';
+UPDATE `address` SET COUNTRY = 'Saint Martin (French part)' WHERE COUNTRY = 'Saint Martin';
+UPDATE `address` SET COUNTRY = 'Slovakia' WHERE COUNTRY = 'Slovakia (Slovak Republic)';
+UPDATE `address` SET COUNTRY = 'Svalbard and Jan Mayen' WHERE COUNTRY = 'Svalbard & Jan Mayen Islands';
+UPDATE `address` SET COUNTRY = 'Tanzania, United Republic of' WHERE COUNTRY = 'Tanzania';
+UPDATE `address` SET COUNTRY = 'United Kingdom of Great Britain and Northern Ireland' WHERE COUNTRY = 'United Kingdom';
+UPDATE `address` SET COUNTRY = 'United States of America' WHERE COUNTRY = 'United States Minor Outlying Islands';
+UPDATE `address` SET COUNTRY = 'Virgin Islands (U.S.)' WHERE COUNTRY = 'United States Virgin Islands';
+UPDATE `address` SET COUNTRY = 'Venezuela (Bolivarian Republic of)' WHERE COUNTRY = 'Venezuela';
+UPDATE `address` SET COUNTRY = 'Viet Nam' WHERE COUNTRY = 'Vietnam';""")
 file.close()
