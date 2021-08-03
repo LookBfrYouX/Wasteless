dc8d3705096fa34c22b3bc65415b1e288b62070b
Maximilian Birzer <mbi47@uclive.ac.nz>
2021-08-04 08:57:10 +1200

    S302T300-286 #time 5m Created search bar component ?implement

---
+++ frontend/src/components/MultiSearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>This be a search bar for multiple keys.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MultiSearchBar"
+}
+</script>
+
+<style scoped>
+
+</style>
