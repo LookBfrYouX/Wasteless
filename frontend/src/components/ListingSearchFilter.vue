109d1bc575108f5f0dc68650bb483623b31ef82b
Alec Fox <amf133@uclive.ac.nz>
2021-08-06 13:28:57 +1200

    S302T300-285 #time 1h fixed some styling issues ?fix ?pair[amf133, rog19] - Rio (css wizard) helped me to style the filter component

--- frontend/src/components/ListingSearchFilter.vue
+++ frontend/src/components/ListingSearchFilter.vue
@@ -1,67 +1,32 @@
 <template>
-  <v-container>
+  <div class="container">
     <v-row align="end">
-      <v-col cols="12" md="4">
-        <h4>Business type</h4>
+      <v-col cols="12" md="3">
+        <v-subheader>Filter businesses</v-subheader>
         <v-select
             v-model="filteredBusinesses"
             :items="businesses"
             label="Select"
             multiple
             solo
+            dense
             prepend-inner-icon="business"
         >
           <template v-slot:selection="{ item, index }">
-            <v-chip v-if="index === 0">
+            <v-chip v-if="index < shownChips" small>
               <span>{{ item }}</span>
             </v-chip>
             <span
-                v-if="index === 1"
+                v-if="index === shownChips"
                 class="grey--text text-caption"
             >
-              (+{{ filteredBusinesses.length - 1 }} others)
+              (+{{ filteredBusinesses.length - shownChips }} others)
             </span>
           </template>
         </v-select>
       </v-col>
-
-      <v-col cols="12" md="4">
-        <h4>Price range</h4>
-        <v-item-group multiple>
-          <v-row align="end">
-            <v-col>
-              <v-text-field label="Min" type="number" min="0.01"
-                            max="10000000" step="0.01" solo></v-text-field>
-            </v-col>
-            <v-col>
-              <v-text-field label="Max" type="number" min="0.01"
-                            max="10000000" step="0.01" solo></v-text-field>
-            </v-col>
-          </v-row>
-        </v-item-group>
-      </v-col>
-
-      <v-col cols="12" md="4">
-        <v-item-group multiple>
-          <v-row align="center">
-            <v-col cols="10">
-              <h4>Listing closes</h4>
-            </v-col>
-            <v-col cols="2" align="end">
-              <v-tooltip bottom>
-                <template v-slot:activator="{ on, attrs }">
-                  <v-icon
-                      v-bind="attrs"
-                      v-on="on"
-                  >
-                    help
-                  </v-icon>
-                </template>
-                <span>One date = earliest, two dates = range</span>
-              </v-tooltip>
-            </v-col>
-          </v-row>
-        </v-item-group>
+      <v-col cols="12" md="3">
+        <v-subheader>Date range</v-subheader>
         <v-menu
             ref="menu"
             v-model="menu"
@@ -80,11 +45,13 @@
                 v-on="on"
                 clearable
                 solo
+                dense
             ></v-text-field>
           </template>
           <v-date-picker
               v-model="dates"
-              multiple
+              range
+              @change="$refs.menu.save(dates)"
           >
             <v-btn
                 text
@@ -104,7 +71,21 @@
         </v-menu>
       </v-col>
     </v-row>
-  </v-container>
+
+    <v-row>
+      <v-col cols="12" md="3">
+        <v-subheader>Min price</v-subheader>
+        <v-text-field label="Min" type="number" min="0.01"
+                      max="10000000" step="0.01" solo dense></v-text-field>
+
+      </v-col>
+      <v-col cols="12" md="3">
+        <v-subheader>Max price</v-subheader>
+        <v-text-field label="Max" type="number" min="0.01"
+                      max="10000000" step="0.01" solo dense></v-text-field>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -112,21 +93,15 @@ export default {
   name: "listingSearchFilter",
   data() {
     return {
-      businesses: ["Accommodation and Food Services", "Retail Trade", "Charitable organisation",
-        "Non-profit organisation"],
+      businesses: ["Hospitality", "Retail", "Charity",
+        "Non-profit"],
       filteredBusinesses: [],
       dates: [],
       min: null,
       max: null,
       menu: false,
+      shownChips: 1,
     }
   },
-  watch: {
-    dates() {
-      if (this.dates.length > 2) {
-        this.dates = this.dates.slice(1).slice(-2)
-      }
-    }
-  }
 }
 </script>
