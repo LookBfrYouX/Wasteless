7ed68e3a14c51aaeb17de0d778d8b496b9708cd2
Maximilian Birzer <mbi47@uclive.ac.nz>
2021-08-11 18:39:21 +1200

    S302T300-296 #time 1m Removed unused validation annotations imports ?fix

--- backend/src/main/java/com/navbara_pigeons/wasteless/entity/Transaction.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/entity/Transaction.java
@@ -9,8 +9,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.DecimalMax;
-import javax.validation.constraints.DecimalMin;
 import lombok.Data;
 import lombok.ToString;
 
