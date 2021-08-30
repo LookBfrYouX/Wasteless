52d400adfabd03622f559a107a6dc46479fc4c93
Maximilian Birzer <mbi47@uclive.ac.nz>
2021-08-30 13:05:26 +1200

    S302T300-364 #time 35m Created Barcode component and added v-text-field ?implement - Created the component - Looked up vuetify library for suitable component - Added v-text-field with placeholder and label text

---
+++ frontend/src/components/BarcodeInput.vue
@@ -0,0 +1,21 @@
+<template>
+<v-form>
+  <label>Barcode</label>
+  <v-text-field label="Barcode No." placeholder="EAN-13 Barcode" v-model="barcode" solo dense />
+</v-form>
+</template>
+
+<script>
+export default {
+  name: "BarcodeInput",
+  data() {
+    return {
+      barcode: "",
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
