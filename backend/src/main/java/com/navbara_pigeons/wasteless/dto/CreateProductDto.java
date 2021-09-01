08c62376d20e7fae02afc4ebf05241a4b0de2d50
Haruka Ichinose <hic21@uclive.ac.nz>
2021-09-01 16:44:46 +1200

    S302T300-372 #time 1h ?implement Updated Create Product DTO to hole nutrition facts.

--- backend/src/main/java/com/navbara_pigeons/wasteless/dto/CreateProductDto.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/dto/CreateProductDto.java
@@ -1,9 +1,14 @@
 package com.navbara_pigeons.wasteless.dto;
 
+import com.navbara_pigeons.wasteless.entity.BusinessType;
 import com.navbara_pigeons.wasteless.entity.Product;
+import com.navbara_pigeons.wasteless.validation.constraints.StringEnumeration;
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import jdk.jfr.BooleanFlag;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
@@ -26,6 +31,44 @@ public class CreateProductDto {
   @DecimalMin(message = "RRP must be above 0.01", value = "0.01")
   @DecimalMax(message = "RRP must be below 10,000,000", value = "10000000.00")
   private Double recommendedRetailPrice;
+
+  @StringEnumeration(enumClass = NutriScore.class, message = "Invalid Nutri-Score given")
+  private String nutriScore;
+
+  @StringEnumeration(enumClass = NovaGroup.class, message = "Invalid Nova Group given")
+  private String novaGroup;
+
+  @StringEnumeration(enumClass = NutritionFactsLevel.class, message = "Invalid fat level given")
+  private String fat;
+
+  @StringEnumeration(enumClass = NutritionFactsLevel.class, message = "Invalid saturated fat level given")
+  private String saturatedFat;
+
+  @StringEnumeration(enumClass = NutritionFactsLevel.class, message = "Invalid sugar level given")
+  private String sugar;
+
+  @StringEnumeration(enumClass = NutritionFactsLevel.class, message = "Invalid salt level given")
+  private String salt;
+
+  @Pattern(regexp = "^true$|^false$|^null$", message = "allowed input: true or false or null")
+  private boolean isGlutenFree;
+
+  @Pattern(regexp = "^true$|^false$|^null$", message = "allowed input: true or false or null")
+  private boolean isDairyFree;
+
+  @Pattern(regexp = "^true$|^false$|^null$", message = "allowed input: true or false or null")
+  private boolean isVegetarian;
+
+  @Pattern(regexp = "^true$|^false$|^null$", message = "allowed input: true or false or null")
+  private boolean isVegan;
+
+  @Pattern(regexp = "^true$|^false$|^null$", message = "allowed input: true or false or null")
+  private boolean isPalmOilFree;
+
+  @NotNull(message = "Business Type is Required")
+  @Length(max = 50, message = "Business Type has to be less than or equal to 50 Characters")
+  @StringEnumeration(enumClass = BusinessType.class, message = "Invalid business type given")
+  private String businessType;
 
 
   public CreateProductDto() {
