17dc47f38dc23b4daff71aa4632e17e407a6c311
Haruka Ichinose <hic21@uclive.ac.nz>
2021-09-07 14:52:06 +1200

    S302T300-422 #time 0m ?test ?pair[rog19, hic21] Added Jest test for emitting selected data to parent component.

--- frontend/test/components/NutritionScoreInput.test.js
+++ frontend/test/components/NutritionScoreInput.test.js
@@ -22,4 +22,18 @@ describe("Nutrition score input test", () => {
     expect(wrapper.vm.$data.options).toEqual(['A', 'B', 'C', 'D', 'E']);
     expect(wrapper.vm.$props.nutritionScore).toEqual(null);
   });
+  test("Score is emitted to parent component", async () => {
+    let inputEl = wrapper.find("input");
+    console.log(inputEl);
+    inputEl.trigger("focus");
+    await wrapper.vm.$nextTick();
+    inputEl.trigger("keydown.space");
+    await wrapper.vm.$nextTick();
+    inputEl.trigger("keydown.down");
+    await wrapper.vm.$nextTick();
+    inputEl.trigger("keydown.enter");
+    await wrapper.vm.$nextTick();
+    expect(wrapper.emitted().input.length).toBe(1);
+    expect(wrapper.emitted().input[0][0]).toBe("A");
+  });
 });
