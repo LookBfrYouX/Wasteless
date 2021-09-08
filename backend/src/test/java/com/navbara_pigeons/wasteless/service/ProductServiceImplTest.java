64cb130001cff97b0eeaa2561db3424fc47ba099
nbt25 <nbt25@uclive.ac.nz>
2021-09-09 09:35:26 +1200

    S302T300-379 #time 1h 30h ?pair[rog19, nbt25] ?test working on replacing commented out tests

--- backend/src/test/java/com/navbara_pigeons/wasteless/service/ProductServiceImplTest.java
+++ backend/src/test/java/com/navbara_pigeons/wasteless/service/ProductServiceImplTest.java
@@ -1,120 +1,84 @@
-//package com.navbara_pigeons.wasteless.service;
-//
-//import com.navbara_pigeons.wasteless.entity.Business;
-//import com.navbara_pigeons.wasteless.entity.Product;
-//import com.navbara_pigeons.wasteless.exception.*;
-//import com.navbara_pigeons.wasteless.testprovider.ServiceTestProvider;
-//import net.minidev.json.JSONObject;
-//import org.junit.BeforeClass;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Disabled;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.security.test.context.support.WithUserDetails;
-//
-//import javax.transaction.Transactional;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//public class ProductServiceImplTest extends ServiceTestProvider {
-//
+package com.navbara_pigeons.wasteless.service;
+
+import com.navbara_pigeons.wasteless.dao.BusinessDao;
+import com.navbara_pigeons.wasteless.dao.ProductDao;
+import com.navbara_pigeons.wasteless.dto.BasicProductDto;
+import com.navbara_pigeons.wasteless.dto.PaginationDto;
+import com.navbara_pigeons.wasteless.entity.Business;
+import com.navbara_pigeons.wasteless.entity.Product;
+import com.navbara_pigeons.wasteless.entity.User;
+import com.navbara_pigeons.wasteless.enums.ProductSortByOption;
+import com.navbara_pigeons.wasteless.exception.*;
+import com.navbara_pigeons.wasteless.helper.PaginationBuilder;
+import com.navbara_pigeons.wasteless.testprovider.ServiceTestProvider;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.data.util.Pair;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+
+public class ProductServiceImplTest extends ServiceTestProvider {
+
+    private final String email = "tony@tony.tony";
+    private final String password = "tonyTony1";
+
+    private final String otherEmail = "test@test.com";
+    private final String otherPassword = "Testtest2";
+
+    private final String productName = "testProduct";
+
+    @Mock
+    private BusinessDao businessDao;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private BusinessService businessService;
+
+    @Mock
+    private ProductDao productDao;
+
+    @InjectMocks
+    private ProductServiceImpl productService;
+    private Long userId;
+    private Long businessId;
+    private Business business;
+
+
+
+    @BeforeEach
+    void beforeEach() throws Exception {
+        // Setting mocks before tests are run to ensure unit testing only
+        User user = makeUser(email, password, false);
+        long userId = user.getId();
+        business = makeBusiness();
+        businessId = business.getId();
+        business.setPrimaryAdministratorId(userId);
+    }
+
 //    @Test
-//    @Transactional
-//    @WithUserDetails(value = "mbi47@uclive.ac.nz")
-//    void getProductsTest() {
-//        try {
-//            // Set up business and product
-//            String testProdName = "testProd";
-//            Business mockBusiness = makeBusiness("testBusiness");
-//            Product mockProduct = makeProduct(testProdName);
-//            mockBusiness.addCatalogueProduct(mockProduct);
-//            // Save and retrieve the business
-//            businessService.saveBusiness(mockBusiness);
-//            List<Product> products = productService.getProducts(Long.toString(mockBusiness.getId()));
-//            // Assert the business has at least one product
-//            assertTrue(products.size() > 0);
-//            // Assert the business has exactly the one product we added to it
-//            assertTrue(products.size() == 1);
-//            // Assert the product has the same name as the one we saved
-//            assertTrue(products.get(0).getName().equals(testProdName));
-//
-//            // Fail test if any unexpected exceptions occur
-//        } catch (Exception exc) {
-//            exc.printStackTrace();
-//            assertTrue(false);
-//        }
+//    @WithMockUser(username = email, password = password)
+//    void getProductsWithAdequatePrivilegesExpectResponse() throws UserNotFoundException, InsufficientPrivilegesException, BusinessNotFoundException, InvalidPaginationInputException {
+//        Product product = makeProduct(productName);
+//        business.addCatalogueProduct(product);
+//        when(userService.isAdmin()).thenReturn(true);
+//        when(businessService.isBusinessAdmin(businessId)).thenReturn(true);
+//        when(productDao.getProducts(any(Business.class), any(PaginationBuilder.class)))
+//                .thenReturn(Pair.of(business.getProductsCatalogue(), Long.valueOf(business.getProductsCatalogue().size())));
+//        PaginationDto<BasicProductDto> products = productService.getProducts(businessId, 0,1, null, true);
+//        Assertions.assertEquals(1, products.getTotalCount());
 //    }
 //
-//    Product findProductWithName(long businessId, String productName) throws BusinessNotFoundException, UserNotFoundException, InsufficientPrivilegesException {
-//        Product savedProduct = null;
-//        for(Product product: productService.getProducts(String.valueOf(businessId))) {
-//            if (product.getName() == productName) {
-//                // Find product with the same name
-//                savedProduct = product;
-//                break;
-//            }
-//        }
-//
-//        assertNotNull(savedProduct, "Couldn't find saved product");
-//        return savedProduct;
-//    }
-//
-//    @Test
-//    @Transactional
-//    @WithUserDetails(value="dnb36@uclive.ac.nz")
-//    void createProductExpectOk() throws UserNotFoundException, BusinessTypeException, BusinessRegistrationException, InsufficientPrivilegesException, BusinessNotFoundException, AddressValidationException {
-//        JSONObject mockProduct = new JSONObject();
-//        final String name = "Pizza";
-//        final double price = 9.99;
-//        final String description = "DESCRIPTION";
-//
-//        Business mockBusiness = makeBusiness("testBusiness");
-//        businessService.saveBusiness(mockBusiness);
-//
-//        mockProduct.put("name", name);
-//        mockProduct.put("recommendedRetailPrice", price);
-//        mockProduct.put("description", description);
-//
-//        assertDoesNotThrow(() -> productService.addProduct(mockBusiness.getId(), mockProduct));
-//
-//        Product savedProduct = findProductWithName(mockBusiness.getId(), name);
-//        assertEquals(name, savedProduct.getName());
-//        assertEquals("NZD", savedProduct.getCurrency());
-//        assertEquals(price, savedProduct.getRecommendedRetailPrice());
-//        assertEquals(description, savedProduct.getDescription());
-//        // Delta between date created and current time less than a second
-//        assertTrue(Math.abs(savedProduct.getCreated().toInstant().toEpochMilli() - System.currentTimeMillis()) < 1000);
-//    }
-//
-//    @Test
-//    @Transactional
-//    @WithUserDetails(value="dnb36@uclive.ac.nz")
-//    void createProductExpectException() {
-//        JSONObject mockProduct = new JSONObject();
-//        mockProduct.put("name", null);
-//
-//        assertThrows(Exception.class, () -> productService.addProduct(1, mockProduct));
-//    }
-//
-//    @Test
-//    @Transactional
-//    @WithUserDetails(value="dnb36@uclive.ac.nz")
-//    void createProductExpectInCatalogue() {
-//        try {
-//            String productName = "Pizza";
-//            JSONObject mockProduct = new JSONObject();
-//            mockProduct.put("name", productName);
-//            Business mockBusiness = makeBusiness("testBusiness");
-//            businessService.saveBusiness(mockBusiness);
-//            assertDoesNotThrow(() -> productService.addProduct(mockBusiness.getId(), mockProduct));
-//            assertEquals(mockBusiness.getProductsCatalogue().get(0).getName(), productName);
-//            // Fail test if any unexpected exceptions occur
-//        } catch (Exception exc) {
-//            exc.printStackTrace();
-//            assertTrue(false);
-//        }
-//    }
-//}
-//
+
+
+}
+
