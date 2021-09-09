c335bb5052bbe51b2bb169236df6d035815f136e
Haruka Ichinose <hic21@uclive.ac.nz>
2021-09-09 18:16:03 +1200

    S302T300-420 #time 10m ?test Modified test after refactoring NovaGroupInput component.

--- frontend/test/components/NovaGroupInput.test.js
+++ frontend/test/components/NovaGroupInput.test.js
@@ -6,11 +6,30 @@ import NovaGroupInput from "@/components/NovaGroupInput";
 let vuetify = new Vuetify();
 let wrapper;
 
+const options = [
+  {
+    text: 'N/A',
+    value: null
+  }, {
+    text: '1',
+    value: '1',
+  }, {
+    text: '2',
+    value: '2',
+  }, {
+    text: '3',
+    value: '3'
+  }, {
+    text: '4',
+    value: '4'
+  }
+];
+
 beforeEach(() => {
   wrapper = mount(NovaGroupInput, {
     vuetify,
     propsData: {
-      novaGroup: null
+      value: null
     }
   });
 });
@@ -19,14 +38,16 @@ afterEach(() => wrapper.destroy());
 
 describe("Nutrition score input test", () => {
   test("Test component is properly mounted", () => {
-    expect(wrapper.vm.$data.options).toEqual(['1', '2', '3', '4']);
-    expect(wrapper.vm.$props.novaGroup).toEqual(null);
+    expect(wrapper.vm.$data.options).toEqual(options);
+    expect(wrapper.vm.$props.value).toEqual(null);
   });
   test("Score is emitted to parent component", async () => {
     let inputEl = wrapper.find("input");
     inputEl.trigger("focus");
     await wrapper.vm.$nextTick();
     inputEl.trigger("keydown.space");
+    await wrapper.vm.$nextTick();
+    inputEl.trigger("keydown.down");
     await wrapper.vm.$nextTick();
     inputEl.trigger("keydown.down");
     await wrapper.vm.$nextTick();
