91ac92462694633d844cf763c7dacd17f9a4e5d2
Rio Ogino <rog19@uclive.ac.nz>
2021-09-10 16:37:28 +1200

    S302T300-365 #time 10m ?implement clean up allergy chips in ListingCard

--- frontend/src/components/cards/ListingCard.vue
+++ frontend/src/components/cards/ListingCard.vue
@@ -1,26 +1,32 @@
 <template>
   <router-link
-      :to="{ name: 'BusinessListingDetail', params: { businessId:item.inventoryItem.business.id, listingId: item.id }}"
-      class="text-decoration-none text-reset"
+    :to="{ name: 'BusinessListingDetail',
+      params: {
+        businessId: item.inventoryItem.business.id,
+        listingId: item.id,
+      },
+    }"
+    class="text-decoration-none text-reset"
   >
     <v-card class="w-100 hover-scale-effect p-relative">
-      <div
-        class="allergy-info-container p-2"
-        v-if="item.inventoryItem.product"
-      >
-        <v-tooltip top
-          v-for="{key, shortText, longText, cls, bg, fg} in [ 
-            { key: 'isDairyFree', shortText: 'DF', longText: 'Dairy Free', cls: null, bg: '#ffdd50', fg: 'black' },
-            { key: 'isGlutenFree', shortText: 'GF', longText: 'Gluten Free', cls: null, bg: '#93641c', fg: 'white' },
-            { key: 'isVegetarian', shortText: 'V', longText: 'Vegetarian', cls: null, bg: '#40826d', fg: 'white' },
-            { key: 'isVegan', shortText: 'VE', longText: 'Vegan', cls: null, bg: '#74B74E', fg: 'white' },
-            { key: 'isPalmOilFree', shortText: 'PO', longText: 'Palm Oil Free', cls: 'palm-oil-free-chip', bg: '#ffc5c5', fg: 'black' }
-          ].filter(({key}) => item.inventoryItem.product[key])"
+      <div class="allergy-info-container p-2" v-if="item.inventoryItem.product">
+        <v-tooltip
+          top
+          v-for="{ key, shortText, longText, chipClass, backgroundColor, foregroundColor }
+                 in allergyChipConfig.filter(({ key }) => item.inventoryItem.product[key])"
           v-on="on"
           :key="key"
         >
           <template v-slot:activator="{ on, attrs }">
-            <v-chip small v-on="on" v-bind="attrs" class="mr-2" :class="cls" :color="bg" :text-color="fg">
+            <v-chip
+              small
+              v-on="on"
+              v-bind="attrs"
+              class="mr-2"
+              :class="chipClass"
+              :color="backgroundColor"
+              :text-color="foregroundColor"
+            >
               <span>
                 {{ shortText }}
               </span>
@@ -29,18 +35,19 @@
           {{ longText }}
         </v-tooltip>
       </div>
+
       <img
           v-if="item.inventoryItem.product.images.length"
           :src="item.inventoryItem.product.images[0].thumbnailFilename"
           alt="Product Image"
           class="image-fluid w-100 m-0"
-      >
+      />
       <img
           v-else
           alt="Product Image"
           class="image-fluid w-100 m-0"
           src="@/../assets/images/default-product-thumbnail.svg"
-      >
+      />
       <v-card-text class="pt-2 pb-0">
         <div class="row">
           <div class="col-6 text-truncate">
@@ -64,7 +71,8 @@
       <v-card-text>
         <div class="row">
           <div class="col-5">
-            {{ $helper.makeCurrencyString(item.price, currency, false) }} <small> for </small>
+            {{ $helper.makeCurrencyString(item.price, currency, false) }}
+            <small> for </small>
             {{ item.quantity }}
           </div>
           <div class="col-7 text-end">
@@ -72,7 +80,6 @@
             {{ $helper.isoToDateString(item.closes) }}
           </div>
         </div>
-
       </v-card-text>
     </v-card>
   </router-link>
@@ -84,26 +91,77 @@ export default {
   data() {
     return {
       currency: null
-    }
+    };
   },
   props: {
     item: Object
   },
-  beforeMount: async function () {
-    return Promise.allSettled(
-        [this.getCurrency()]);
+
+  beforeMount: function () {
+    this.getCurrency();
   },
+
   methods: {
     /**
      * Gets currency object.
      * @returns {Promise<void>} Currency object, null when the currency doesn't exist or API request error.
      */
     getCurrency: async function () {
-      this.currency = await this.$helper.getCurrencyForBusinessByCountry(
-          this.item.inventoryItem.business.address.country);
-    }
-  }
-}
+      this.currency = this.$helper.getCurrencyForBusinessByCountry(
+        this.item.inventoryItem.business.address.country
+      );
+    },
+  },
+
+  computed: {
+    /**
+     * Information for chip color, text, classes etc.
+     */
+    allergyChipConfig() {
+      return [
+        {
+          key: "isDairyFree",
+          shortText: "DF",
+          longText: "Dairy Free",
+          chipClass: null,
+          backgroundColor: "#ffdd50",
+          foregroundColor: "black",
+        }, {
+          key: "isGlutenFree",
+          shortText: "GF",
+          longText: "Gluten Free",
+          chipClass: null,
+          backgroundColor: "#93641c",
+          foregroundColor: "white",
+        }, {
+          key: "isVegetarian",
+          shortText: "V",
+          longText: "Vegetarian",
+          chipClass: null,
+          backgroundColor: "#40826d",
+          foregroundColor: "white",
+        }, {
+          key: "isVegan",
+          shortText: "VE",
+          longText: "Vegan",
+          chipClass: null,
+          backgroundColor: "#74B74E",
+          foregroundColor: "white",
+        }, {
+          key: "isPalmOilFree",
+          shortText: "PO",
+          longText: "Palm Oil Free",
+          // custom CSS for palm oil free chip
+          // if foreground color not black, should be changed in the CSS
+          // as well for the inline SVG
+          chipClass: "palm-oil-free-chip",
+          backgroundColor: "#ffc5c5",
+          foregroundColor: "black",
+        }
+      ];
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
