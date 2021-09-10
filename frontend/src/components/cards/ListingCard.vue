1430cc7e79097ccac3030487ed01125352f273f7
Rio Ogino <rog19@uclive.ac.nz>
2021-09-10 16:09:10 +1200

    S302T300-365 #time 45m ?implement chips for allergy info in listing cards
    Currently have updated listings.vue to force all chips on all listings.
    
    Listings currently have no unique background colors, just grey

--- frontend/src/components/cards/ListingCard.vue
+++ frontend/src/components/cards/ListingCard.vue
@@ -3,7 +3,32 @@
       :to="{ name: 'BusinessListingDetail', params: { businessId:item.inventoryItem.business.id, listingId: item.id }}"
       class="text-decoration-none text-reset"
   >
-    <v-card class="w-100 hover-scale-effect">
+    <v-card class="w-100 hover-scale-effect p-relative">
+      <div
+        class="allergy-info-container p-2"
+        v-if="item.inventoryItem.product"
+      >
+        <v-tooltip top
+          v-for="{key, shortText, longText, cls} in [ 
+            { key: 'isDairyFree', shortText: 'DF', longText: 'Dairy Free', cls: null },
+            { key: 'isGlutenFree', shortText: 'GF', longText: 'Gluten Free', cls: null },
+            { key: 'isVegetarian', shortText: 'V', longText: 'Vegetarian', cls: null },
+            { key: 'isVegan', shortText: 'VE', longText: 'Vegan', cls: null },
+            { key: 'isPalmOilFree', shortText: 'PO', longText: 'Palm Oil Free', cls: 'palm-oil-free-chip' }
+          ].filter(({key}) => item.inventoryItem.product[key])"
+          v-on="on"
+          :key="key"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-chip small v-on="on" v-bind="attrs" :class="cls">
+              <span>
+                {{ shortText }}
+              </span>
+            </v-chip>
+          </template>
+          {{ longText }}
+        </v-tooltip>
+      </div>
       <img
           v-if="item.inventoryItem.product.images.length"
           :src="item.inventoryItem.product.images[0].thumbnailFilename"
@@ -97,5 +122,31 @@ export default {
 
 .listing-container img {
   max-height: 30vh;
+}
+
+.allergy-info-container {
+  position: absolute;
+}
+
+.allergy-info-container .v-chip {
+  margin-left: 0.3rem;
+}
+.palm-oil-free-chip span {
+  position: relative;
+  line-height: 0.7;
+}
+
+.palm-oil-free-chip span::after {
+  // https://stackoverflow.com/a/28974253/5204356
+  background: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' preserveAspectRatio='none' viewBox='0 0 100 100'><path d='M0 100 L100 0 L100 10 L10 100' fill='black' /></svg>");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: 100% 100%, auto;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  top: 0;
+  content: "";
 }
 </style>
