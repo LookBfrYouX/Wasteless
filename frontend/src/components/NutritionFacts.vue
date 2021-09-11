f219781db27400c1d2285353bd1b130058c1a03a
Haruka Ichinose <hic21@uclive.ac.nz>
2021-09-11 18:00:05 +1200

    S302T300-352 #time 1h 15m ?implement Created NutritionFacts component.

---
+++ frontend/src/components/NutritionFacts.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-expansion-panels>
+    <v-expansion-panel>
+      <v-expansion-panel-header>
+        Nutrition Facts
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <v-row>
+          <v-col cols="12" md="6">
+            <div class="d-flex justify-space-around">
+              <v-img
+                  contain
+                  max-width="30%"
+                  src="https://picsum.photos/id/11/500/300">
+              </v-img>
+              <v-img
+                  contain
+                  max-width="50%"
+                  src="https://picsum.photos/id/11/500/300">
+              </v-img>
+            </div>
+          </v-col>
+          <v-col cols="12" md="6">
+            Second column
+          </v-col>
+        </v-row>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+
+<script>
+export default {
+  name: "NutritionFacts"
+}
+</script>
+
+<style scoped>
+
+</style>
