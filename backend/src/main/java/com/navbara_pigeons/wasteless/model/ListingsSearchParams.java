d749a66463713c316fe71954e32eff471c33d7a2
Rio Ogino <rog19@uclive.ac.nz>
2021-09-13 10:47:17 +1200

    S302T300-409 #time 10m ?implement add nutrient levels to ListingsSearchParams

--- backend/src/main/java/com/navbara_pigeons/wasteless/model/ListingsSearchParams.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/model/ListingsSearchParams.java
@@ -3,6 +3,8 @@ package com.navbara_pigeons.wasteless.model;
 import com.navbara_pigeons.wasteless.entity.BusinessType;
 import com.navbara_pigeons.wasteless.enums.ListingSearchKeys;
 import com.navbara_pigeons.wasteless.enums.ListingSortByOption;
+import com.navbara_pigeons.wasteless.enums.NutritionFactsLevel;
+
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +34,12 @@ public class ListingsSearchParams {
   private Double maxPrice;
   private List<ZonedDateTime> filterDates;
   private List<BusinessType> businessTypes;
-  // returns all listings by default
+
+  private List<NutritionFactsLevel> fat;
+  private List<NutritionFactsLevel> saturatedFat;
+  private List<NutritionFactsLevel> sugars;
+  private List<NutritionFactsLevel> salt;
+
   private int DEFAULT_PAGE_END_INDEX = 1000000000;
 
   /**
@@ -51,7 +58,6 @@ public class ListingsSearchParams {
    * @param pagStartIndex
    */
   public void setPagStartIndex(Integer pagStartIndex) {
-    System.out.println(pagStartIndex);
     if (pagStartIndex == null) {
       this.pagStartIndex = 0;
     } else {
@@ -124,6 +130,57 @@ public class ListingsSearchParams {
       this.sortBy = ListingSortByOption.valueOf("created");
     } else {
       this.sortBy = sortBy;
+    }
+  }
+
+  /**
+   * Sets the allowed fat levels. null means the filter will not be applied
+   * @param fat fat levels. If the array is empty it is set to null
+   */
+  public void setFat(List<NutritionFactsLevel> fat) {
+    if (fat == null || fat.isEmpty()) {
+      this.fat = null;
+    } else {
+      this.fat = fat;
+    }
+  }
+
+
+  /**
+   * Sets the allowed saturated fat levels. null means the filter will not be applied
+   * @param saturatedFat saturatedFat levels. If the array is empty it is set to null
+   */
+  public void setSaturatedFat(List<NutritionFactsLevel> saturatedFat) {
+    if (saturatedFat == null || saturatedFat.isEmpty()) {
+      this.saturatedFat = null;
+    } else {
+      this.saturatedFat = saturatedFat;
+    }
+  }
+
+
+  /**
+   * Sets the allowed sugar levels. null means the filter will not be applied
+   * @param sugars sugar levels. If the array is empty it is set to null
+   */
+  public void setSugars(List<NutritionFactsLevel> sugars) {
+    if (sugars == null || sugars.isEmpty()) {
+      this.sugars = null;
+    } else {
+      this.sugars = sugars;
+    }
+  }
+
+
+  /**
+   * Sets the allowed salt levels. null means the filter will not be applied
+   * @param salt salt levels. If the array is empty it is set to null
+   */
+  public void setSalt(List<NutritionFactsLevel> salt) {
+    if (salt == null || salt.isEmpty()) {
+      this.salt = null;
+    } else {
+      this.salt = salt;
     }
   }
 }
