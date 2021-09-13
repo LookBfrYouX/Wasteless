39e3e886c11c9e316bea131d3a951c64159c4ff0
Haruka Ichinose <hic21@uclive.ac.nz>
2021-09-13 17:34:48 +1200

    S302T300-352 #time 1h 30m ?implement Implemented all nutrition facts display section with hardcoded value.

--- frontend/src/components/NutritionFacts.vue
+++ frontend/src/components/NutritionFacts.vue
@@ -2,26 +2,85 @@
   <v-expansion-panels>
     <v-expansion-panel>
       <v-expansion-panel-header>
-        Nutrition Facts
+        <h5 class="text-muted">
+          Nutrition Facts
+        </h5>
       </v-expansion-panel-header>
       <v-expansion-panel-content>
         <v-row>
           <v-col cols="12" md="6">
-            <div class="d-flex justify-space-around">
+            <div class="d-flex justify-space-around mb-4">
               <v-img
                   contain
-                  max-width="30%"
-                  src="https://picsum.photos/id/11/500/300">
+                  max-width="20%"
+                  :src="novaGroupImgSrc">
               </v-img>
               <v-img
                   contain
-                  max-width="50%"
-                  src="https://picsum.photos/id/11/500/300">
+                  max-width="60%"
+                  :src="nutriScoreImgSrc">
               </v-img>
             </div>
+            <div>
+              Dietary Requirements
+            </div>
+            <div>
+              <v-chip>
+                Gluten Free
+              </v-chip>
+            </div>
+
           </v-col>
           <v-col cols="12" md="6">
-            Second column
+            <div>
+              Nutrients Levels
+            </div>
+            <div>
+              <v-list>
+                <v-list-item-group>
+                  <v-list-item class="pl-0">
+                    <v-list-item-icon>
+                      <span class="material-icons red--text">
+                        circle
+                      </span>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>Fat</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item class="pl-0">
+                    <v-list-item-icon>
+                      <span class="material-icons yellow--text">
+                        circle
+                      </span>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>Saturated Fat</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item class="pl-0">
+                    <v-list-item-icon>
+                      <span class="material-icons green--text">
+                        circle
+                      </span>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>Sugars</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item class="pl-0">
+                    <v-list-item-icon>
+                      <span class="material-icons yellow--text">
+                        circle
+                      </span>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>Salt</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </div>
           </v-col>
         </v-row>
       </v-expansion-panel-content>
@@ -30,11 +89,23 @@
 </template>
 
 <script>
+import novaGroup1Image from "@/../assets/images/nova-group-1.svg";
+import nutriScoreAImage from "@/../assets/images/nutriscore_A.png";
+
 export default {
-  name: "NutritionFacts"
+  name: "NutritionFacts",
+
+  data() {
+    return {
+      novaGroupImgSrc: novaGroup1Image,
+      nutriScoreImgSrc: nutriScoreAImage
+    }
+  }
 }
+
 </script>
-
-<style scoped>
-
+<style>
+div.v-list-item {
+  pointer-events: none;
+}
 </style>
