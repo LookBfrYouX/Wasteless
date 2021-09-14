5fd4d6fdf767fc291efa9ee8f13de4e355a6b39a
Rio Ogino <rog19@uclive.ac.nz>
2021-09-15 11:27:59 +1200

    S302T300-363 #time 35m ?test filtering by nutrition levels
    Not an ideal situation with data.sql - I was missing product with ID 5002 and the listing IDs don't really match up with the product IDs, and although I could update them to match this will cause other tests to break.
    
    The only alternative I can think of is to nuke the database after each test and add the users, businesses, products, inventory items and listings we need for each test, but that would take several hours to program and isn't worth it.

--- backend/src/test/java/com/navbara_pigeons/wasteless/dao/Specifications/ListingSpecificationsTest.java
+++ backend/src/test/java/com/navbara_pigeons/wasteless/dao/Specifications/ListingSpecificationsTest.java
@@ -14,6 +14,7 @@ import com.navbara_pigeons.wasteless.model.ListingsSearchParams;
 import com.navbara_pigeons.wasteless.service.InventoryService;
 import com.navbara_pigeons.wasteless.testprovider.MainTestProvider;
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +22,7 @@ import org.springframework.data.jpa.domain.Specification;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
@@ -88,14 +90,55 @@ public class ListingSpecificationsTest extends MainTestProvider {
         assertEquals(5002, results.get(0).getId());
     }
 
+    /**
+     * Test that the addNutritionLevelPredicate works. Can't figure out to check if the Predicate objects it creates are the right one
+     * and still need to test it being integrated with the filtering method, so this tests both at the same time. Couldn't figure out
+     * how to test the meetsSearchCriteria method without calling the database unfortunately
+     *
+     * Recommend using the below query to figure out what the correct response should be:
+     * SELECT listing.ID, product.ID AS PRODUCT_ID,
+     *   product.FAT,
+     *   product.SATURATED_FAT,
+     *   product.SUGARS,
+     *   product.SALT
+     *  FROM listing
+     *   JOIN inventory_item ON
+     *      listing.INVENTORY_ITEM_ID = inventory_item.ID
+     *    JOIN product ON
+     *    	inventory_item.PRODUCT_ID = product.ID
+     *
+     *    ORDER BY listing.ID
+     *
+     *
+     */
     @Test
-    void resultsMeetSearchCriteriaTestFilteredByFat() {
+    void resultsMeetSearchCriteriaTestFilteredByFatMultipleLevels() {
         listingsSearchParams.setFat(List.of(NutritionFactsLevel.MODERATE, NutritionFactsLevel.HIGH));
         Specification<Listing> specification = ListingSpecifications.meetsSearchCriteria(listingsSearchParams);
         List<Listing> results = listingDao.findAll(specification);
         assertEquals(2, results.size());
+        assertEquals(List.of(5006, 5007).toString(), results.stream().map(el -> el.getId()).sorted().collect(Collectors.toList()).toString());
+    }
 
+
+    @Test
+    void resultsMeetSearchCriteriaTestFilteredBySaturatedFatOneLevel() {
+        listingsSearchParams.setSaturatedFat(List.of(NutritionFactsLevel.LOW));
+        Specification<Listing> specification = ListingSpecifications.meetsSearchCriteria(listingsSearchParams);
+        List<Listing> results = listingDao.findAll(specification);
+        System.out.println(results.stream().map(el -> el.getId()).sorted().collect(Collectors.toList()).toString());
+        assertEquals(2, results.size());
+        assertEquals(List.of(5003, 5005).toString(), results.stream().map(el -> el.getId()).sorted().collect(Collectors.toList()).toString());
+    }
+
+
+    @Test
+    void resultsMeetSearchCriteriaTestFilteredByMultipleNutritionFieldsSugarsAndSalt() {
+        listingsSearchParams.setSalt(List.of(NutritionFactsLevel.LOW));
+        listingsSearchParams.setSugars(List.of(NutritionFactsLevel.MODERATE));
+        Specification<Listing> specification = ListingSpecifications.meetsSearchCriteria(listingsSearchParams);
+        List<Listing> results = listingDao.findAll(specification);
+        assertEquals(1, results.size());
         assertEquals(5005, results.get(0).getId());
-        assertEquals(5007, results.get(0).getId());
     }
 }
