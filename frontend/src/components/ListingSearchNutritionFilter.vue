db52b82bf8674f07c1c4784bffaa0154ff610a6a
Alec Fox <amf133@uclive.ac.nz>
2021-09-15 15:29:46 +1200

    S302T300-416 #time 1h 20m added diets and nova group dropdowns ?implement - Added dropdowns to the - Currently these have minimal functionality to the listing search nutrition filter component

---
+++ frontend/src/components/ListingSearchNutritionFilter.vue
@@ -0,0 +1,168 @@
+<template>
+  <v-expansion-panels>
+    <v-expansion-panel>
+      <v-expansion-panel-header>
+        <h5 class="text-muted">
+          Nutrition Facts
+        </h5>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <v-row>
+          <v-col cols="12" lg="6" class="my-0 py-0">
+            <v-subheader>Diets</v-subheader>
+            <v-select
+                v-model="selected"
+                :items=diets
+                label="Select"
+                multiple
+                solo
+                dense
+                item-text="short"
+                item-value="long"
+            >
+              <template v-slot:selection="{ item, index }">
+                <v-chip v-if="index < shownChips" small>
+                  <span>{{ item }}</span>
+                </v-chip>
+                <span
+                    v-if="index === shownChips"
+                    class="grey--text text-caption"
+                >
+              (+{{ selected.length - shownChips }} others)
+            </span>
+              </template>
+            </v-select>
+          </v-col>
+
+          <v-col cols="6" lg="3" class="my-0 py-0">
+            <v-subheader>Min NovaGroup</v-subheader>
+            <v-select
+                :items=novaGroups
+                label="Select"
+                multiple
+                solo
+                dense
+                item-text="short"
+                item-value="long"
+            >
+            </v-select>
+          </v-col>
+
+          <v-col cols="6" lg="3" class="my-0 py-0">
+            <v-subheader>Max NovaGroup</v-subheader>
+            <v-select
+                :items=novaGroups
+                label="Select"
+                multiple
+                solo
+                dense
+                item-text="short"
+                item-value="long"
+            >
+            </v-select>
+          </v-col>
+
+          <v-col cols="6" lg="3" class="my-0 py-0">
+            <v-subheader>Fat</v-subheader>
+            <v-select
+                :items=nutritionOptions
+                label="Select"
+                multiple
+                solo
+                dense
+                item-text="short"
+                item-value="long"
+            >
+            </v-select>
+          </v-col>
+
+          <v-col cols="6" lg="3" class="my-0 py-0">
+            <v-subheader>Saturated fat</v-subheader>
+            <v-select
+                :items=nutritionOptions
+                label="Select"
+                multiple
+                solo
+                dense
+                item-text="short"
+                item-value="long"
+            >
+            </v-select>
+          </v-col>
+
+          <v-col cols="6" lg="3" class="my-0 py-0">
+            <v-subheader>Min NutriScore</v-subheader>
+            <v-select
+                :items=nutriScoreOptions
+                label="Select"
+                multiple
+                solo
+                dense
+                item-text="short"
+                item-value="long"
+            >
+            </v-select>
+          </v-col>
+
+          <v-col cols="6" lg="3" class="my-0 py-0">
+            <v-subheader>Max NutriScore</v-subheader>
+            <v-select
+                :items=nutriScoreOptions
+                label="Select"
+                multiple
+                solo
+                dense
+                item-text="short"
+                item-value="long"
+            >
+            </v-select>
+          </v-col>
+
+          <v-col cols="6" lg="3" class="my-0 py-0">
+            <v-subheader>Sugars</v-subheader>
+            <v-select
+                :items=nutritionOptions
+                label="Select"
+                multiple
+                solo
+                dense
+                item-text="short"
+                item-value="long"
+            >
+            </v-select>
+          </v-col>
+
+          <v-col cols="6" lg="3" class="my-0 py-0">
+            <v-subheader>Salt</v-subheader>
+            <v-select
+                :items=nutritionOptions
+                label="Select"
+                multiple
+                solo
+                dense
+                item-text="short"
+                item-value="long"
+            >
+            </v-select>
+          </v-col>
+        </v-row>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+
+<script>
+export default {
+  name: "ListingSearchNutritionFilter",
+  data() {
+    return {
+      shownChips: 1,
+      novaGroups: [1, 2, 3, 4],
+      selected: [],
+      nutriScoreOptions: ['A', 'B', 'C', 'D', 'E'],
+      nutritionOptions: ['Unknown', 'Low', 'Moderate', 'High'],
+      diets: ['Gluten Free', 'Dairy Free', 'Vegetarian', 'Vegan', 'Palm Oil Free'],
+    }
+  },
+}
+</script>
