d387b7e887ada3c7973e724b1056826d35af1265
nbt25 <nbt25@uclive.ac.nz>
2021-09-16 13:30:57 +1200

    S302T300-419 #time 1m ?test deleted unecessary cucumber test file

--- backend/src/test/java/com/navbara_pigeons/wasteless/cucumber/EU3SearchListingsByNutritionInfoStepDefs.java
+++ backend/src/test/java/com/navbara_pigeons/wasteless/cucumber/EU3SearchListingsByNutritionInfoStepDefs.java
@@ -2,19 +2,27 @@ package com.navbara_pigeons.wasteless.cucumber;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.navbara_pigeons.wasteless.dto.CreateProductDto;
+import com.navbara_pigeons.wasteless.entity.Product;
+import com.navbara_pigeons.wasteless.enums.NutritionFactsLevel;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Iterator;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 public class EU3SearchListingsByNutritionInfoStepDefs extends CucumberTestProvider{
     private MvcResult response;
+
 
     @BeforeEach
     void setup() {
@@ -22,27 +30,27 @@ public class EU3SearchListingsByNutritionInfoStepDefs extends CucumberTestProvid
     }
 
     @When("I send a valid request to {string} with fat set to {string}")
-    void i_send_a_valid_request_to_with_fat_set_to(String endPointUrl, String fatValue) throws Exception {
+    public void iSendAValidRequestToWithFatSetTo(String endpointUrl, String fatValue) throws Exception {
         this.response = mockMvc.perform(
-                get(endPointUrl)
+                get(endpointUrl)
                         .param("fat", fatValue)
         ).andReturn();
         Assertions.assertNotNull(this.response);
     }
 
     @Then("only the products with fat set to {string} are shown")
-    void only_the_products_with_fat_set_to_are_shown(String fatValue) throws UnsupportedEncodingException, JsonProcessingException {
+    public void onlyTheProductsWithFatSetToAreShown(String fatValue) throws UnsupportedEncodingException, JsonProcessingException {
         JsonNode jsonResponse = objectMapper.readTree(this.response.getResponse().getContentAsString());
         Iterator<JsonNode> results = jsonResponse.get("results").elements();
         while (results.hasNext()) {
-            Assertions.assertTrue(results.next().get("fat").asText().equalsIgnoreCase(fatValue));
+            Assertions.assertTrue(results.next().get("inventoryItem").get("product").get("fat").asText().equalsIgnoreCase(fatValue));
         }
     }
 
     @When("I send a valid request to {string} with saturatedFat set to {string} and {string}")
-    void i_send_a_valid_request_to_with_saturated_fat_set_to_and(String endPointUrl, String saturatedFatValue1, String saturatedFatValue2) throws Exception {
+    public void iSendAValidRequestToWithSaturatedFatSetToAnd(String endpointUrl, String saturatedFatValue1, String saturatedFatValue2) throws Exception {
         this.response = mockMvc.perform(
-                get(endPointUrl)
+                get(endpointUrl)
                         .param("pagStartIndex", "0")
                         .param("pagEndIndex", "3")
                         .param("saturatedFat", saturatedFatValue1)
@@ -52,18 +60,18 @@ public class EU3SearchListingsByNutritionInfoStepDefs extends CucumberTestProvid
     }
 
     @Then("only the products with saturatedFat set to {string} and {string} are shown")
-    void only_the_products_with_saturated_fat_set_to_and_are_shown(String saturatedFatValue1, String saturatedFatValue2) throws UnsupportedEncodingException, JsonProcessingException {
+    public void onlyTheProductsWithSaturatedFatSetToAndAreShown(String saturatedFatValue1, String saturatedFatValue2) throws UnsupportedEncodingException, JsonProcessingException {
         JsonNode jsonResponse = objectMapper.readTree(this.response.getResponse().getContentAsString());
         Iterator<JsonNode> results = jsonResponse.get("results").elements();
         while (results.hasNext()) {
-            Assertions.assertTrue(results.next().get("saturatedFat").asText().equalsIgnoreCase(saturatedFatValue1) || results.next().get("saturatedFat").asText().equalsIgnoreCase(saturatedFatValue2));
+            Assertions.assertTrue(results.next().get("inventoryItem").get("product").get("saturatedFat").asText().equalsIgnoreCase(saturatedFatValue1) || results.next().get("inventoryItem").get("product").get("saturatedFat").asText().equalsIgnoreCase(saturatedFatValue2));
         }
     }
 
     @When("I send a valid request to {string} with salt set to {string}")
-    void i_send_a_valid_request_to_with_salt_set_to(String endPointUrl, String saltValue) throws Exception {
+    public void iSendAValidRequestToWithSaltSetTo(String endpointUrl, String saltValue) throws Exception {
         this.response = mockMvc.perform(
-                get(endPointUrl)
+                get(endpointUrl)
                         .param("pagStartIndex", "0")
                         .param("pagEndIndex", "3")
                         .param("salt", saltValue)
@@ -72,18 +80,19 @@ public class EU3SearchListingsByNutritionInfoStepDefs extends CucumberTestProvid
     }
 
     @Then("only the products with salt set to {string} are shown")
-    void onlyTheProductsWithSaltSetToAreShown(String saltValue) throws UnsupportedEncodingException, JsonProcessingException {
+    public void onlyTheProductsWithSaltSetToAreShown(String saltValue) throws UnsupportedEncodingException, JsonProcessingException {
         JsonNode jsonResponse = objectMapper.readTree(this.response.getResponse().getContentAsString());
         Iterator<JsonNode> results = jsonResponse.get("results").elements();
         while (results.hasNext()) {
-            Assertions.assertTrue(results.next().get("salt").asText().equalsIgnoreCase(saltValue));
+            Assertions.assertTrue(results.next().get("inventoryItem").get("product").get("salt").asText().equalsIgnoreCase(saltValue));
         }
     }
 
+
     @When("I send a valid request to {string} with  sugars set to {string}")
-    void i_send_a_valid_request_to_with_sugars_set_to(String endPointUrl, String sugarsValue) throws Exception {
+    public void iSendAValidRequestToWithSugarsSetTo(String endpointUrl, String sugarsValue) throws Exception {
         this.response = mockMvc.perform(
-                get(endPointUrl)
+                get(endpointUrl)
                         .param("pagStartIndex", "0")
                         .param("pagEndIndex", "3")
                         .param("sugars", sugarsValue)
@@ -92,11 +101,11 @@ public class EU3SearchListingsByNutritionInfoStepDefs extends CucumberTestProvid
     }
 
     @Then("only the products with sugars set to {string} are shown")
-    void only_the_products_with_sugars_set_to_are_shown(String sugarsValue) throws UnsupportedEncodingException, JsonProcessingException {
+    public void onlyTheProductsWithSugarsSetToAreShown(String sugarsValue) throws UnsupportedEncodingException, JsonProcessingException {
         JsonNode jsonResponse = objectMapper.readTree(this.response.getResponse().getContentAsString());
         Iterator<JsonNode> results = jsonResponse.get("results").elements();
         while (results.hasNext()) {
-            Assertions.assertTrue(results.next().get("sugars").asText().equalsIgnoreCase(sugarsValue));
+            Assertions.assertTrue(results.next().get("inventoryItem").get("product").get("sugars").asText().equalsIgnoreCase(sugarsValue));
         }
     }
 }
