888503cb56a50946c624cb4a1b96e834cc90bb05
nbt25 <nbt25@uclive.ac.nz>
2021-09-20 10:44:43 +1200

    S302T300-390 #time 1h 10m ?implement planned out how sales report table was going to work - got familiar with how vuetify data tables work

---
+++ frontend/src/components/SalesReportTable.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-data-table>
+
+  </v-data-table>
+</template>
+
+<script>
+export default {
+props:{
+  granularity:"day",
+  dateRange:{
+    minDate: "",
+    maxDate: ""
+  }
+},
+data:{
+  totalTransactions:0,
+  totalSales:0,
+
+}
+
+}
+</script>
+
+<style>
+
+</style>
