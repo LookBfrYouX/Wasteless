aa9e4376e0addfaa2e01ccf5d43946c079828c98
nbt25 <nbt25@uclive.ac.nz>
2021-09-21 09:55:07 +1200

    S302T300-390 #time 25m ?implement finished making the table component includes placeholder names as don't know what the backend is going to send through yet. Also includes logic for showing table only if there are transactions for the business. May need to include a loading animation for this

--- frontend/src/components/SalesReportTable.vue
+++ frontend/src/components/SalesReportTable.vue
@@ -1,24 +1,41 @@
 <template>
-  <v-data-table>
-
-  </v-data-table>
+  <div>
+    <div v-if="transactionInformation.length != 0">
+      <v-data-table
+        :headers="headers"
+        :items="transactionInformation"
+        :items-per-page="5">
+      </v-data-table>
+    </div>
+    <div>
+      No Transactions for this business
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-props:{
-  granularity:"day",
-  dateRange:{
-    minDate: "",
-    maxDate: ""
+  props: {
+  granularity: "",
+  transactionInformation: {}
+  },
+  data() {
+    return {
+      headers: [
+      {
+        text: granularity,
+        value: "date"
+      },
+      {
+        text: "Transactions",
+        value: "numOfTransactions"
+      },
+      {
+        text: "Sales",
+        value: "transactionValue"
+      }]
+    }
   }
-},
-data:{
-  totalTransactions:0,
-  totalSales:0,
-
-}
-
 }
 </script>
 
