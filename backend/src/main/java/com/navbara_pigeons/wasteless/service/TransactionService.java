b113d9f6c3f6f54b410f327a7a81150247def5cf
Alec Fox <amf133@uclive.ac.nz>
2021-09-22 11:03:52 +1200

    S302T300-353 #time 30m implemented method in transaction service ?implement - Added getTransactionData in transaction service - Replied to comment on separate MR

--- backend/src/main/java/com/navbara_pigeons/wasteless/service/TransactionService.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/service/TransactionService.java
@@ -1,8 +1,17 @@
 package com.navbara_pigeons.wasteless.service;
 
+import com.navbara_pigeons.wasteless.dto.TransactionDataDto;
 import com.navbara_pigeons.wasteless.entity.Transaction;
+import com.navbara_pigeons.wasteless.exception.BusinessNotFoundException;
+import com.navbara_pigeons.wasteless.exception.InsufficientPrivilegesException;
+import com.navbara_pigeons.wasteless.exception.UserNotFoundException;
+import java.time.ZonedDateTime;
 
 public interface TransactionService {
 
   void saveTransaction(Transaction transaction);
+
+  TransactionDataDto getTransactionData(Long businessId, ZonedDateTime startSaleDate,
+      ZonedDateTime endSaleDate, String granularity)
+      throws UserNotFoundException, BusinessNotFoundException, InsufficientPrivilegesException;
 }
--- backend/src/main/java/com/navbara_pigeons/wasteless/service/TransactionServiceImpl.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/service/TransactionServiceImpl.java
@@ -1,7 +1,13 @@
 package com.navbara_pigeons.wasteless.service;
 
 import com.navbara_pigeons.wasteless.dao.TransactionDao;
+import com.navbara_pigeons.wasteless.dao.TransactionDaoHibernate;
+import com.navbara_pigeons.wasteless.dto.TransactionDataDto;
 import com.navbara_pigeons.wasteless.entity.Transaction;
+import com.navbara_pigeons.wasteless.exception.BusinessNotFoundException;
+import com.navbara_pigeons.wasteless.exception.InsufficientPrivilegesException;
+import com.navbara_pigeons.wasteless.exception.UserNotFoundException;
+import java.time.ZonedDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +18,9 @@ import org.springframework.stereotype.Service;
 public class TransactionServiceImpl implements TransactionService {
 
   TransactionDao transactionDao;
+  TransactionDaoHibernate transactionDaoHibernate;
+  BusinessService businessService;
+  UserService userService;
 
   /**
    * TransactionServiceImpl constructor that takes autowired parameters and sets up the service for
@@ -20,8 +29,13 @@ public class TransactionServiceImpl implements TransactionService {
    * @param transactionDao The Transaction Data Access Object.
    */
   @Autowired
-  public TransactionServiceImpl(TransactionDao transactionDao) {
+  public TransactionServiceImpl(TransactionDao transactionDao,
+      TransactionDaoHibernate transactionDaoHibernate, BusinessService businessService,
+      UserService userService) {
     this.transactionDao = transactionDao;
+    this.transactionDaoHibernate = transactionDaoHibernate;
+    this.businessService = businessService;
+    this.userService = userService;
   }
 
   /**
@@ -29,8 +43,35 @@ public class TransactionServiceImpl implements TransactionService {
    *
    * @param transaction Passed in Transaction model
    */
+  @Override
   public void saveTransaction(Transaction transaction) {
-    transactionDao.save(transaction).getId();
+    transactionDao.save(transaction);
   }
 
+  /**
+   * Method to get transactional data from a business with given id
+   *
+   * @param businessId    Id of the business to query transactions from
+   * @param startSaleDate Start of date range to query by
+   * @param endSaleDate   End of date range to query by
+   * @param granularity   Granularity to group by dates
+   * @return TransactionDataDto with transaction data
+   * @throws UserNotFoundException           Thrown when user who made request is not found in db
+   * @throws BusinessNotFoundException       Thrown businessId doesn't match any business
+   * @throws InsufficientPrivilegesException Thrown when user does not have access to query these
+   *                                         transactions
+   */
+  @Override
+  public TransactionDataDto getTransactionData(Long businessId, ZonedDateTime startSaleDate,
+      ZonedDateTime endSaleDate, String granularity)
+      throws UserNotFoundException, BusinessNotFoundException, InsufficientPrivilegesException {
+    if (!businessService.isBusinessAdmin(businessId) && !userService.isAdmin()) {
+      throw new InsufficientPrivilegesException(
+          "User does not have permission to retrieve transactions from business with id "
+              + businessId);
+    }
+
+    return this.transactionDaoHibernate.getTransactionData(businessId, startSaleDate, endSaleDate,
+        granularity);
+  }
 }
