36e10339b154a59fcd9f515ecd50dbfca1fffc49
Rio Ogino <rog19@uclive.ac.nz>
2021-09-22 20:52:32 +1200

    S302T300-426 #time 30m ?implement extract start/end date transformation in transaction controller
    Extracted the start/end date transformation for better testability.
    
    Made a request in postman and got some errors with he controller converting the string to a LocalDate which I still haven't figured out

--- backend/src/main/java/com/navbara_pigeons/wasteless/controller/TransactionController.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/controller/TransactionController.java
@@ -1,23 +1,21 @@
 package com.navbara_pigeons.wasteless.controller;
 
-import com.navbara_pigeons.wasteless.dto.TransactionDataDto;
 import com.navbara_pigeons.wasteless.enums.TransactionGranularity;
 import com.navbara_pigeons.wasteless.exception.BusinessNotFoundException;
 import com.navbara_pigeons.wasteless.exception.InsufficientPrivilegesException;
 import com.navbara_pigeons.wasteless.exception.UserNotFoundException;
-import com.navbara_pigeons.wasteless.service.ProductService;
 import com.navbara_pigeons.wasteless.service.TransactionService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
@@ -40,27 +38,42 @@ public class TransactionController {
     this.transactionService = transactionService;
   }
 
-
-  @GetMapping("/businesses/{id}/transactions")
-  @Operation(summary = "View a business's transaction history", description = "Return revenue/items sold count for a business in a given date range, grouped by a day, week, month or year")
-  public ResponseEntity<Object> viewTransactionHistory(
-      @Parameter(
-        description = "The unique ID number of the business"
-      ) @PathVariable long id,
-      @Parameter(
-        description = "Start date"
-      ) @RequestParam(required = false) LocalDate startDate,
-      @Parameter(
-          description = "End date"
-      ) @RequestParam(required = false) LocalDate endDate
-    ) throws UserNotFoundException, InsufficientPrivilegesException, BusinessNotFoundException {
+  /**
+   * Transforms the LocalDate used as method argument to a ZonedDateTime required by the service
+   * @param startDate start date. May be null
+   * @param endDate end date. May be null
+   * @return pair of dates transformed so that:
+   *  - Start date set to minimum date if not given, and time to start of day
+   *  - End date set to the current date if not given
+   *    - End time is transformed to last second
+   *  - Times are in UTC time as that is what is used elsewhere in the backend. There is no concept of a
+   *    local timezone for businesses or users so it makes the most sense to just not send the time in the frontend
+   */
+  private Pair<ZonedDateTime, ZonedDateTime> transformDateRange(LocalDate startDate, LocalDate endDate) {
     if (startDate == null) startDate = LocalDate.MIN;
     if (endDate == null) endDate = LocalDate.now(ZoneOffset.UTC);
     ZonedDateTime transformedStartDate = startDate.atStartOfDay(ZoneOffset.UTC);
-    ZonedDateTime   transformedEndDate = endDate.plusDays(1).atStartOfDay(ZoneOffset.UTC);
+    ZonedDateTime   transformedEndDate = endDate.plusDays(1).atStartOfDay(ZoneOffset.UTC).minusSeconds(1);
+    return Pair.of(transformedStartDate, transformedEndDate);
+  }
 
+  @GetMapping("/businesses/{id}/transactions")
+  @Operation(
+      summary = "View a business's transaction history",
+      description =
+          "Return revenue/items sold count for a business in a given date range, grouped by a day, week, month or year")
+  public ResponseEntity<Object> viewTransactionHistory(
+      @Parameter(description = "The unique ID number of the business") @PathVariable long id,
+      @Parameter(description = "Start date for transaction history. Time set to start of day in the UTC timezone")
+          @RequestParam(required = false)
+          LocalDate startDate,
+      @Parameter(description = "End date for transaction history. Time set to end of day in the UTC timezone")
+          @RequestParam(required = false)
+          LocalDate endDate)
+      throws UserNotFoundException, InsufficientPrivilegesException, BusinessNotFoundException {
+    Pair<ZonedDateTime, ZonedDateTime> dateRange = transformDateRange(startDate, endDate);
     return new ResponseEntity<>(
-        transactionService.getTransactionData(id, transformedStartDate, transformedEndDate, TransactionGranularity.DAY),
+        transactionService.getTransactionData(id, dateRange.getLeft(), dateRange.getRight(), TransactionGranularity.DAY),
         HttpStatus.OK
     );
   }
