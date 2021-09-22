6a5f9355d03e5ae2be6c925f74e76f92b64a9e43
Alec Fox <amf133@uclive.ac.nz>
2021-09-22 13:02:51 +1200

    S302T300-353 #time 5m fixed issue found in MR ?fix - Statement checks if any transactions were found to prevent Null pointer exceptions

--- backend/src/main/java/com/navbara_pigeons/wasteless/dao/TransactionDaoHibernateImpl.java
+++ backend/src/main/java/com/navbara_pigeons/wasteless/dao/TransactionDaoHibernateImpl.java
@@ -60,6 +60,10 @@ public class TransactionDaoHibernateImpl implements TransactionDaoHibernate {
     // Elements 0 and 1 have totalTransactionCount and amount in them respectively
     List<Object[]> totals = totalsQuery.getResultList();
 
+    // If no transactions were found, return empty TransactionDataDto
+    if (results.isEmpty() || totals.isEmpty()) {
+      return new TransactionDataDto(new ArrayList<>(), 0, 0.00);
+    }
     List<TransactionReportModel> transactionReportModels = new ArrayList<>();
     for (Object[] result : results) {
       ZonedDateTime date = ZonedDateTime.parse(result[0].toString());
