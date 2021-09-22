7deed48df98521a53c37d9456d6595707ae946f7
nbt25 <nbt25@uclive.ac.nz>
2021-09-23 09:30:01 +1200

    S302T300-432 #time 1h 30m ?implement Sales Report page added api request method and setting mock values also figured out how to reload the table with values if the granularity or min and max dates change

--- frontend/src/views/business/SalesReport.vue
+++ frontend/src/views/business/SalesReport.vue
@@ -1,40 +1,104 @@
 <template>
-  <div class="w-100">
-    <div class="row">
-      <div class="col">
-        Date Range: <v-btn>Select Date Range</v-btn>
-      </div>
-      <div class="col">
-        Group By: <v-select v-model="granularity" :items="items" label="Group By" solo/>
-      </div>
-    </div>
+  <v-container class="w-100">
+    <v-row>
+      <v-col class="d-flex align-center justify-center">
+        <v-subheader>Date Range</v-subheader>
+        <v-btn label="Date Range">Select Date Range</v-btn>
+      </v-col>
+      <v-col align="center" class="d-flex align-center justify-center">
+        <v-subheader>Granularity</v-subheader>
+        <v-select class="granularitySelect" v-model="granularity" dense="true" :items="items" label="Group By" solo/>
+      </v-col>
+      <v-col align="center" class="d-flex align-center justify-center">
+        <v-btn v-on:click="setResultsWithMocks">Go</v-btn>
+      </v-col>
+    </v-row>
     <v-divider></v-divider>
-    <div class="row">
-
-    </div>
-  </div>
-
+    <v-row>
+      <v-col>
+        <h4>Total Transactions</h4>
+        {{numberOfTransactions}}
+      </v-col>
+      <v-col>
+        <h4>Total Sales</h4>
+        {{totalValue}}
+      </v-col>
+      <v-col>
+        <h4>Period</h4>
+        {{minDate}} To {{maxDate}}
+      </v-col>
+      <v-col>
+        <h4>Business</h4>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import {Api} from "@/Api"
 export default {
   data() {
     return {
       minDate:"",
       maxDate:"",
+      businessName:"",
       granularity:"",
       totalValue:0,
       numberOfTransactions:0,
       transactionData:{},
-      items: ["Day", "Month","Year"]
+      items: ["Day", "Month","Year"],
+      mockTransactionResponse:{
+        totalAmount: 400,
+        totalTransactionCount: 10,
+        transactions: [
+          {"date":"2020-01-02 15:34:20",
+           "transactionCount": 5,
+           "amount": 200},
+          {"date":"2021-01-22 15:34:20",
+           "transactionCount": 5,
+           "amount": 200},
+        ]
+      }
     }
   },
   methods: {
-
-  }
-};
+    /**
+     * Sends API request and sets totalvalue, numberOfTransactions and transactionData variables
+     */
+    query: async function () {
+      /* makes a query to the api to retrieve the transactions with the props*/
+      try {
+        const response = (await Api.getTransactions(this.$stateStore.getters.getBusinessId,{granularity:this.granularity,
+                                                                                            minDate:this.minDate,
+                                                                                            maxDate:this.maxDate})).data;
+        this.totalValue = response.totalAmount;
+        this.numberOfTransactions = response.totalTransactionCount;
+        this.transactionData = response.transactions;
+      } catch (err) {
+        if (await Api.handle401.call(this, err)) {
+          return;
+        }
+        this.apiErrorMessage = err.userFacingErrorMessage;
+      }
+    },
+    /**
+     * Sends API request and sets totalvalue, numberOfTransactions and transactionData variables
+     */
+    setResultsWithMocks: function () {
+      /* makes a query to the api to retrieve the transactions with the props*/
+      this.totalValue = this.mockTransactionResponse.totalAmount;
+      this.numberOfTransactions = this.mockTransactionResponse.totalTransactionCount;
+      this.transactionData = this.mockTransactionResponse.transactions;
+    }
+  },
+}
 </script>
 
 <style>
-
+.v-text-field__details {
+  display: none;
+}
+.granularitySelect {
+  max-width: 204.467px;
+}
 </style>
