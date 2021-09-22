ca7de27167c3dad2f591ee00fc2cfd4c5204b3b3
Fletcher Dick <fdi19@uclive.ac.nz>
2021-09-22 15:03:56 +1200

    S302T300-347 #time 50m Created Route, new Page and refactored details styling ?implement

---
+++ frontend/src/views/business/Transactions.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Hey! You're on the new Transactions Page 🧾
+  </div>
+</template>
+<script>
+
+export default {
+  name: "Transactions",
+  props: {
+    businessId: {
+      required: true,
+      type: Number
+    },
+  },
+  computed: {
+    /**
+     * Computes the total number of pages for the pagination component.
+     */
+    totalPages: function () {
+      return Math.floor((this.totalResults - 1) / this.itemsPerPage) + 1;
+    }
+  },
+};
+</script>
