7250c27429b8de5fec7167578b32933abc8b1abc
nbt25 <nbt25@uclive.ac.nz>
2021-09-28 19:22:03 +1300

    S302T300-404 #time 30m ?implement put array serialisation into helper

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -399,42 +399,7 @@ export const Api = {
     return instance.get('/listings/search',
         {
           params: parameters,
-          paramsSerializer: params => {
-            const parts = [];
-
-            const encode = val => {
-              return encodeURIComponent(val).replace(/%3A/gi, ':')
-              .replace(/%24/g, '$')
-              .replace(/%2C/gi, ',')
-              .replace(/%20/g, '+')
-              .replace(/%5B/gi, '[')
-              .replace(/%5D/gi, ']');
-            }
-
-            const convertPart = (key, val) => {
-              if (val instanceof Date) {
-                val = val.toISOString()
-              } else if (val instanceof Object) {
-                val = JSON.stringify(val)
-              }
-
-              parts.push(encode(key) + '=' + encode(val));
-            }
-
-            Object.entries(params).forEach(([key, val]) => {
-              if (val === null || typeof val === 'undefined') {
-                return
-              }
-
-              if (Array.isArray(val)) {
-                val.forEach((v) => convertPart(`${key}`, v))
-              } else {
-                convertPart(key, val)
-              }
-            })
-
-            return parts.join('&')
-          }
+          paramsSerializer: params => { this.$helper.arraySerializer(params)}
         }).catch(err => {
       throw ApiRequestError.createFromMessageMap(err, {
         400: 'Invalid pagination parameters sent',
