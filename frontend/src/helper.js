01f15390c729755a7b45279afab5d4d970cecea6
Rio Ogino <rog19@uclive.ac.nz>
2021-09-28 19:45:22 +1300

    S302T300-404 #time 10m ?pair[rog19, nbt25] ?fix array serialization and it a bit nicer
    Now in its own function

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -329,7 +329,6 @@ export const helper = {
       return null;
     }
   },
-
   /**
    * Formats object with name components, removing null or empty strings
    * @param {{firstName: String, middleName: String, lastName: String, nickname: String}} object with name components
@@ -345,42 +344,5 @@ export const helper = {
       name += ` (${user.nickname})`
     }
     return name;
-  },
-
-  arraySerializer(params) {
-    const parts = [];
-
-    const encode = val => {
-      return encodeURIComponent(val).replace(/%3A/gi, ':')
-      .replace(/%24/g, '$')
-      .replace(/%2C/gi, ',')
-      .replace(/%20/g, '+')
-      .replace(/%5B/gi, '[')
-      .replace(/%5D/gi, ']');
-    }
-
-    const convertPart = (key, val) => {
-      if (val instanceof Date) {
-        val = val.toISOString()
-      } else if (val instanceof Object) {
-        val = JSON.stringify(val)
-      }
-
-      parts.push(encode(key) + '=' + encode(val));
-    }
-
-    Object.entries(params).forEach(([key, val]) => {
-      if (val === null || typeof val === 'undefined') {
-        return
-      }
-
-      if (Array.isArray(val)) {
-        val.forEach((v) => convertPart(`${key}`, v))
-      } else {
-        convertPart(key, val)
-      }
-    })
-
-    return parts.join('&')
   }
 }
