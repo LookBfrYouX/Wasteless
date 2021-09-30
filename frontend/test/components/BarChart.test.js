2d403c341be0d6a2a8c073d3c8ad03bd9954666c
Maximilian Birzer <mbi47@uclive.ac.nz>
2021-09-30 14:13:39 +1300

    S302T300-441 #time 20m Added jest test for bar chart component mounting ?test - Tested to ensure component mounts properly.

---
+++ frontend/test/components/BarChart.test.js
@@ -0,0 +1,27 @@
+import {shallowMount} from "@vue/test-utils"
+import BarChart from "@/components/charts/BarChart";
+
+let wrapper;
+
+beforeEach(() => {
+    HTMLCanvasElement.prototype.getContext = jest.fn(() => true);
+    wrapper = shallowMount(BarChart, {
+        propsData: {
+            chartData: {
+                label: 'Test',
+                backgroundColor: '#2b39a1',
+                yAxisID: 'Sales',
+                data: [1, 2, 3],
+            },
+            options: {
+                responsive: true,
+            }
+        }
+    });
+});
+
+describe("BarChart test", () => {
+    test("Test component is properly mounted", () => {
+        expect(wrapper.vm.$props.chartData).toBeDefined();
+    });
+});
